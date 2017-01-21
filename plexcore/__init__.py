ab59fc184ba1656b37e6c263c9feb3334e69bd7c
Tanim Islam <tanim.islam@gmail.com>
2017-01-21 14:40:06 -0800

    Changed code so that app.db database in ~/.config/plexstuff/ directory instead of resources directory.
    Changes to be committed:
    	modified:   plexcore/__init__.py
    	deleted:    resources/app.db

--- plexcore/__init__.py
+++ plexcore/__init__.py
@@ -1,16 +1,17 @@
 from . import plexinitialization
 pi = plexinitialization.PlexInitialization( )
 
-import os, sys, xdg
+import os, sys, xdg.BaseDirectory
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 # resource file
 mainDir = os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) )
+baseConfDir = xdg.BaseDirectory.save_config_path( 'plexstuff' )
 
 # follow directions in http://pythoncentral.io/introductory-tutorial-python-sqlalchemy/
-_engine = create_engine( 'sqlite:///%s' % os.path.join( mainDir, 'resources', 'app.db') )
+_engine = create_engine( 'sqlite:///%s' % os.path.join( baseConfDir, 'app.db') )
 Base = declarative_base( )
 Base.metadata.bind = _engine
 session = sessionmaker( bind = _engine )( )

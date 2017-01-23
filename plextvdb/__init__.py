7fa956a51bf9b57cd9d044a7021a4abc48218a8f
Tanim Islam <tanim.islam@gmail.com>
2017-01-23 10:56:21 -0800

    Refactoring common code to go into __init__.py
    Changes to be committed:
    	modified:   __init__.py
    	modified:   plextvdb.py
    	modified:   plextvdb_gui.py

--- plextvdb/__init__.py
+++ plextvdb/__init__.py
@@ -1,3 +1,19 @@
 # resource file
 import os
 mainDir = os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) )
+
+_apiKey = '0B3F6D72213D71C8'
+_usrKey = 'AEE839E62568BA63'
+_usname = 'tanimislam1978'
+
+def get_token( verify = True ):
+    data = { 'apikey' : _apiKey,
+             'username' : _usname,
+             'userkey' : _usrKey }
+    headers = { 'Content-Type' : 'application/json' }
+    response = requests.post( 'https://api.thetvdb.com/login',
+                              data = json.dumps( data ),
+                              verify = verify, headers = headers )
+    if response.status_code != 200:
+        return None
+    return response.json( )[ 'token' ]

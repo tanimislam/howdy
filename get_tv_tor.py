e6f8cbf8933d4a5dda8120f6b322b12ce546fd6a
Tanim Islam <tanim.islam@gmail.com>
2017-02-04 17:38:12 -0800

    Changes to be committed: 	modified:   get_tv_tor.py

--- get_tv_tor.py
+++ get_tv_tor.py
@@ -4,54 +4,37 @@ import re, codecs
 from optparse import OptionParser
 from plextvdb import plextvdb_torrents
 
-def get_tv_torrent_tpb( name, maxnum = 10, doAny = False, raiseError = False,
-                        filename = None ):
+def get_items_tpb(name, maxnum = 10, doAny = False, raiseError = False):
     assert( maxnum >= 5 )
     its, status = plextvdb_torrents.get_tv_torrent_tpb( name, maxnum = maxnum, doAny = doAny )
     if status != 'SUCCESS':
         if raiseError:
             raise ValueError('ERROR, COULD NOT FIND %s.' % name)
         print 'ERROR, COULD NOT FIND %s.' % name
-        return
+        return None
     items = [ { 'title' : item[0], 'seeders' : item[1], 'leechers' : item[2], 'link' : item[3] } for
               item in its ]
-    if len( items ) != 1:
-        sortdict = { idx + 1 : item for ( idx, item ) in enumerate(items) }
-        bs = codecs.encode( 'Choose TV episode or series:\n%s\n' %
-                            '\n'.join(map(lambda idx: '%d: %s (%d SE, %d LE)' % ( idx, sortdict[ idx ][ 'title' ],
-                                                                                  sortdict[ idx ][ 'seeders' ],
-                                                                                  sortdict[ idx ][ 'leechers' ]),
-                                          sorted( sortdict ) ) ), 'utf-8' )
-        iidx = raw_input( bs )
-        try:
-            iidx = int( iidx.strip( ) )
-            if iidx not in sortdict:
-                print('Error, need to choose one of the TV files. Exiting...')
-                return
-            magnet_link = sortdict[ iidx ][ 'link' ]
-            actmov = sortdict[ iidx ][ 'title' ]
-        except Exception:
-            print('Error, did not give a valid integer value. Exiting...')
-            return
-    else:
-        actmov = max( items )[ 'title' ]
-        magnet_link = max( items )[ 'link' ]
+    return items
 
-    print('Chosen TV show: %s' % actmov )
-    if filename is None:
-        print('magnet link: %s' % magnet_link )
-    else:
-        with open(filename, 'w') as openfile:
-            openfile.write('%s\n' % magnet_link )
-
-def get_tv_torrent_torrentz( name, maxnum = 10, filename = None ):
+def get_items_torrentz( name, maxnum = 10 ):
     assert( maxnum >= 5 )
     its, status = plextvdb_torrents.get_tv_torrent_torrentz( name, maxnum = maxnum )
     if status != 'SUCCESS':
         print 'ERROR, COULD NOT FIND %s.' % name
-        return
+        return None
     items = [ { 'title' : item[0], 'seeders' : item[1], 'leechers' : item[2], 'link' : item[3] } for
               item in its ]
+    return items
+
+def get_items_rarbg( name, maxnum = 10 ):
+    assert( maxnum >= 10 )
+    items, status = plextvdb_torrents.get_tv_torrent_rarbg( name, maxnum = maxnum )
+    if status != 'SUCCESS':        
+        print 'ERROR, COULD NOT FIND %s.' % name
+        return None
+    return items
+
+def get_tv_torrent_items( items, filename = None ):
     if len( items ) != 1:
         sortdict = { idx + 1 : item for ( idx, item ) in enumerate(items) }
         bs = codecs.encode( 'Choose TV episode or series:\n%s\n' %
@@ -93,8 +76,11 @@ if __name__=='__main__':
                       help = 'If defined, put option into filename.')
     opts, args = parser.parse_args( )
     assert( opts.name is not None )
-    try:
-        get_tv_torrent_tpb( opts.name, doAny = opts.do_any, maxnum = opts.maxnum, raiseError = True, filename = opts.filename )
-    except ValueError:
-        get_tv_torrent_torrentz( opts.name, maxnum = opts.maxnum,
-                                 filename = opts.filename )
+    #
+    items = get_items_tpb( opts.name, doAny = opts.do_any, maxnum = opts.maxnum, raiseError = False )
+    if items is None:
+        items = get_items_torrentz( opts.name, maxnum = opts.maxnum )
+    if items is None:
+        items = get_items_rarbg( opts.name, maxnum = opts.maxnum )
+    if items is not None:
+        get_tv_torrent_items( items, filename = opts.filename )

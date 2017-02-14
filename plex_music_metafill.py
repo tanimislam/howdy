bd2babceb9ec32fe3788732638b741af67083b43
Tanim Islam <tanim.islam@gmail.com>
2017-02-13 19:54:53 -0800

    Added in a new file, "plex_music_metafill.py", which fills out music metadata given a song title and artist.
    Changes to be committed:
    	new file:   plex_music_metafill.py
    	modified:   plexmusic/plexmusic.py

---
+++ plex_music_metafill.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from plexmusic import plexmusic
+from optparse import OptionParser
+from cStringIO import StringIO
+from PIL import Image
+import mutagen.mp4, os, requests
+
+def fill_m4a_metadata( filename, artist_name, song_name ):
+    assert( os.path.isfile( filename ) )
+    assert( os.path.basename( filename ).lower( ).endswith( '.m4a' ) )
+    #
+    ## now start it off
+    pm = plexmusic.PlexMusic( )
+    data_dict, status = pm.get_music_metadata( song_name = song_name,
+                                               artist_name = artist_name )
+    if status != 'SUCCESS':
+        print 'ERROR, %s' % status
+        return
+    mp4tags = mutagen.mp4.MP4( filename )
+    mp4tags[ '\xa9nam' ] = [ data_dict[ 'song' ], ]
+    mp4tags[ '\xa9alb' ] = [ data_dict[ 'album' ], ]
+    mp4tags[ '\xa9ART' ] = [ data_dict[ 'artist' ], ]
+    mp4tags[ 'aART' ] = [ data_dict[ 'artist' ], ]
+    mp4tags[ '\xa9day' ] = [ str(data_dict[ 'year' ]), ]
+    mp4tags[ 'trkn' ] = [ ( data_dict[ 'tracknumber' ],
+                            data_dict[ 'total tracks' ] ), ]
+    if data_dict[ 'album url' ] != '':
+        csio = StringIO( requests.get( data_dict[ 'album url' ] ).content )
+        img = Image.open( csio )
+        csio2 = StringIO( )
+        img.save( csio2, format = 'png' )
+        mp4tags[ 'covr' ] = [ mutagen.mp4.MP4Cover( csio2.getvalue( ), mutagen.mp4.MP4Cover.FORMAT_PNG ), ]
+        csio.close( )
+        csio2.close( )
+    mp4tags.save( )
+
+if __name__=='__main__':
+    parser = OptionParser( )
+    parser.add_option( '--inputfile', dest='filename', type=str, action='store',
+                       help = 'Name of the M4A file to write metadata to.' )
+    parser.add_option( '--song', dest='song_name', type=str, action='store',
+                       help = 'Name of the song to put into the M4A file.' )
+    parser.add_option( '--artist', dest='artist_name', type=str, action='store',
+                       help = 'Name of the artist to put into the M4A file.' )
+    opts, args = parser.parse_args( )
+    assert(all(map(lambda tok: tok is not None,
+                   ( opts.filename, opts.song_name, opts.artist_name ) ) ) )
+    fill_m4a_metadata( opts.filename, opts.artist_name, opts.song_name )
--- plexmusic/plexmusic.py
+++ plexmusic/plexmusic.py
@@ -1,4 +1,4 @@
-import os, sys, glob, numpy
+import os, sys, glob, numpy, titlecase
 from . import mainDir, pygn
 try:
     from ConfigParser import RawConfigParser
@@ -33,5 +33,41 @@ def get_gracenote_credentials( ):
         raise ValueError("Error, conf file does not have userID.")
     return cParser.get( "GRACENOTE", "clientID" ), cParser.get( "GRACENOTE", "userID" )
 
-def get_all_stuff( ):
-    pass
+class PlexMusic( object ):
+    def __init__( self ):
+        self.clientID, self.userID = get_gracenote_credentials( )
+
+    def get_music_metadata( self, song_name, artist_name ):
+        metadata_song = pygn.search( clientID = self.clientID, userID = self.userID,
+                                     artist = titlecase.titlecase( artist_name ),
+                                     track = titlecase.titlecase( song_name ) )
+        if titlecase.titlecase( metadata_song['track_title'] ) != \
+           titlecase.titlecase( song_name ):
+            return None, "ERROR, COULD NOT FIND ARTIST = %s, SONG = %s." % (
+                titlecase.titlecase( artist_name ), titlecase.titlecase( song_name ) )
+        #
+        ## now see if I can get the album name
+        if 'album_title' not in metadata_song:
+            return None, "ERROR, COULD NOT FIND ALBUM FOR ARTIST = %s, SONG = %s." % (
+                titlecase.titlecase( artist_name ), titlecase.titlecase( song_name ) )
+        if 'track_number' not in metadata_song:
+            return None, "ERROR, COULD NOT FIND TRACK NUMBER FOR ARTIST = %s, SONG = %s." % (
+                titlecase.titlecase( artist_name ), titlecase.titlecase( song_name ) )
+        track_number = int( metadata_song[ 'track_number' ] )
+        album_title = metadata_song[ 'album_title' ]
+        metadata_album = pygn.search(clientID = self.clientID, userID = self.userID,
+                                     artist = titlecase.titlecase( artist_name ),
+                                     album = album_title )
+        total_tracks = len( metadata_album['tracks'] )
+        album_url = ''
+        if 'album_art_url' in metadata_album:
+            album_url = metadata_album[ 'album_art_url' ]
+        album_year = int( metadata_album[ 'album_year' ] )
+        data_dict = { 'song' : titlecase.titlecase( song_name ),
+                      'artist' : titlecase.titlecase( artist_name ),
+                      'tracknumber' : track_number,
+                      'total tracks' : total_tracks,
+                      'year' : album_year,
+                      'album url' : album_url,
+                      'album' : titlecase.titlecase( album_title ) }
+        return data_dict, 'SUCCESS'

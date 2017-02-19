134791db73bc98eb324ca20e5a3e3edc453874af
Tanim Islam <tanim.islam@gmail.com>
2017-02-19 08:19:29 -0800

    Integrated in YouTube service into plexstuff plexmusic.
    Changes to be committed:
    	modified:   plex_music_metafill.py
    	modified:   plexcore/plexcore.py
    	modified:   plexmusic/__init__.py
    	modified:   plexmusic/plexmusic.py

--- plex_music_metafill.py
+++ plex_music_metafill.py
@@ -1,39 +1,7 @@
 #!/usr/bin/env python2
 
-from plexmusic import plexmusic
+from plexmusic.plexmusic import fill_m4a_metadata
 from optparse import OptionParser
-from cStringIO import StringIO
-from PIL import Image
-import mutagen.mp4, os, requests
-
-def fill_m4a_metadata( filename, artist_name, song_name ):
-    assert( os.path.isfile( filename ) )
-    assert( os.path.basename( filename ).lower( ).endswith( '.m4a' ) )
-    #
-    ## now start it off
-    pm = plexmusic.PlexMusic( )
-    data_dict, status = pm.get_music_metadata( song_name = song_name,
-                                               artist_name = artist_name )
-    if status != 'SUCCESS':
-        print 'ERROR, %s' % status
-        return
-    mp4tags = mutagen.mp4.MP4( filename )
-    mp4tags[ '\xa9nam' ] = [ data_dict[ 'song' ], ]
-    mp4tags[ '\xa9alb' ] = [ data_dict[ 'album' ], ]
-    mp4tags[ '\xa9ART' ] = [ data_dict[ 'artist' ], ]
-    mp4tags[ 'aART' ] = [ data_dict[ 'artist' ], ]
-    mp4tags[ '\xa9day' ] = [ str(data_dict[ 'year' ]), ]
-    mp4tags[ 'trkn' ] = [ ( data_dict[ 'tracknumber' ],
-                            data_dict[ 'total tracks' ] ), ]
-    if data_dict[ 'album url' ] != '':
-        csio = StringIO( requests.get( data_dict[ 'album url' ] ).content )
-        img = Image.open( csio )
-        csio2 = StringIO( )
-        img.save( csio2, format = 'png' )
-        mp4tags[ 'covr' ] = [ mutagen.mp4.MP4Cover( csio2.getvalue( ), mutagen.mp4.MP4Cover.FORMAT_PNG ), ]
-        csio.close( )
-        csio2.close( )
-    mp4tags.save( )
 
 if __name__=='__main__':
     parser = OptionParser( )

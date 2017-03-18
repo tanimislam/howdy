1bbc2ef8e3768e2fdf3e46cc8fe7b8f4fef23beb
Tanim Islam <tanim.islam@gmail.com>
2017-03-17 17:28:08 -0700

    Added TMDB related stuff for whatever wasn't working with TVDB (since TVDB API v2 is down).
    Changes to be committed:
    	modified:   plextvdb_other.py

--- plextvdb/plextvdb_other.py
+++ plextvdb/plextvdb_other.py
@@ -1,7 +1,9 @@
-import requests, os, sys, json, re, logging, datetime
+import requests, os, sys, json, re
+import logging, datetime, fuzzywuzzy.fuzz
 from PIL import Image
 from cStringIO import StringIO
 from dateutil.relativedelta import relativedelta
+from plexstuff.plextmdb import tmdb_apiKey
 
 def get_series_omdb_id( series_name ):
     params = { 's' : series_name, 'type' : 'series', 'plot' : 'full' }
@@ -109,4 +111,90 @@ def get_tot_epdict_omdb( showName, inYear = None ):
         title = episode[ 'Title' ]
         tot_epdict.setdefault( seasnum, { } )
         tot_epdict[seasnum][epno] = title
+    return tot_epdict
+
+def get_possible_tmdb_ids( series_name, firstAiredYear = None ):
+    params = { 'api_key' : tmdb_apiKey, 'query' : '+'.join( series_name.split( ) ) }
+    if firstAiredYear is not None:
+        params[ 'first_air_date_year' ] = firstAiredYear
+    response = requests.get( 'https://api.themoviedb.org/3/search/tv',
+                             params = params, verify = False )
+    if response.status_code != 200:
+        return None
+    data = response.json( )
+    total_pages = data[ 'total_pages' ]
+    results = filter(lambda result: fuzzywuzzy.fuzz.ratio( result['name'], series_name ) >= 10.0,
+                     data['results'] )
+    results = sorted( results, key = lambda result: -fuzzywuzzy.fuzz.ratio( result['name'], series_name ) )
+    if total_pages >= 2:
+        for pageno in xrange(2, max( 5, total_pages + 1 ) ):
+            params = { 'api_key' : tmdb_apiKey,
+                       'query' : '+'.join( series_name.split( ) ),
+                       'page' : pageno }
+            if firstAiredYear is not None:
+                params[ 'first_air_date_year' ] = firstAiredYear
+            response = requests.get( 'https://api.themoviedb.org/3/search/tv',
+                                     params = params, verify = False )
+            if response.status_code != 200:
+                continue
+            data = response.json( )
+            newresults = filter(lambda result: fuzzywuzzy.fuzz.ratio( result['title'], title ) >= 10.0,
+                                data['results'] )
+            if len( newresults ) > 0:
+                results += sorted( newresults, key = lambda result: -fuzzywuzzy.fuzz.ratio( result['title'], title ) )
+    if len( results ) == 0: return None
+    return map(lambda result: { 'id' : result['id'], 'name' : result['name'],
+                                'airedYear' : datetime.datetime.strptime( result['first_air_date'], '%Y-%m-%d' ).year },
+               results )
+
+def get_series_tmdb_id( series_name, firstAiredYear = None ):
+    results = get_possible_tmdb_ids( series_name, firstAiredYear = firstAiredYear )
+    if len( results ) == 0: return None
+    return results[0]['id']
+
+"""
+Ignore specials (season 0) for now...
+"""
+def get_episodes_series_tmdb( tmdbID, fromDate = None ):
+    response = requests.get( 'https://api.themoviedb.org/3/tv/%d' % tmdbID,
+                             params = { 'api_key' : tmdb_apiKey }, verify = False )
+    if response.status_code != 200:
+        return None
+    data = response.json( )
+    currentDate = datetime.datetime.now( ).date( )
+    sData = [ ]
+    for season_elem in data[ 'seasons' ]:
+        if season_elem['season_number'] == 0: continue
+        season_number = season_elem['season_number']
+        response_season = requests.get( 'https://api.themoviedb.org/3/tv/%d/season/%d' % ( tmdbID, season_number ),
+                                        params = { 'api_key' : tmdb_apiKey }, verify = False )
+        if response_season.status_code != 200: continue
+        data_season = response_season.json( )
+        for episode in data_season[ 'episodes' ]:
+            try:
+                date = datetime.datetime.strptime( episode['air_date'], '%Y-%m-%d' ).date( )
+                if date >= currentDate:
+                    continue
+                if fromDate is not None:
+                    if date < fromDate:
+                        continue
+                sData.append( { 'name' : episode['name'],
+                                'airedDate' : date,
+                                'season' : season_number,
+                                'episode' : episode['episode_number'] } )
+            except:
+                continue
+    return sData
+
+def get_tot_epdict_tmdb( showName, firstAiredYear = None ):
+    tmdbID = get_series_tmdb_id( showName, firstAiredYear = firstAiredYear )
+    if tmdbID is None: return None
+    eps = get_episodes_series_tmdb( tmdbID )
+    tot_epdict = { }
+    for episode in eps:
+        seasnum = episode[ 'season' ]
+        title = episode[ 'name' ]
+        epno = episode[ 'episode' ]
+        tot_epdict.setdefault( seasnum, { } )
+        tot_epdict[ seasnum ][ epno ] = title
     return tot_epdict

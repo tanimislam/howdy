ad2de92e0bdb2f10b4bdf1a8d7664e13af567397
Tanim Islam <tanim.islam@gmail.com>
2017-11-08 16:45:49 -0800

    Enhancement to plex_music_album.py, getting a song listing
    Changes to be committed:
    	modified:   plex_music_album.py

--- plex_music_album.py
+++ plex_music_album.py
@@ -11,9 +11,17 @@ if __name__=='__main__':
                        help = 'Name of the artist to get album image for.' )
     parser.add_option( '--album', dest='album_name', type=str, action='store',
                        help = 'Name of the album to get album image for.' )
+    parser.add_option( '--songs', dest='do_songs', action='store_true', default = False,
+                       help = 'If chosen, get the song listing instead of downloading the album image.')
     opts, args = parser.parse_args( )
     assert(all(map(lambda tok: tok is not None, ( opts.artist_name, opts.album_name ) ) ) )
     #
-    ##
+    ##    
     pm = plexmusic.PlexMusic( )
-    pm.get_song_image( opts.artist_name, opts.album_name )
+    if not opts.do_songs:
+        pm.get_song_image( opts.artist_name, opts.album_name )
+    else:
+        track_listing = pm.get_song_listing( opts.artist_name, album_name = opts.album_name )
+        print track_listing
+        for title, trkno in track_listing:
+            print '%02d - %s' % ( trkno, title )

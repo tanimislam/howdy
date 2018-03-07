28352227f8af84cf29a92409fce2897f494818e3
Tanim Islam <tanim.islam@gmail.com>
2018-03-07 08:55:00 -0800

    plex_music_album.py now has a --songs --formatted functionality that prints the list of songs into a format recognized by plex_music_metafill.py

--- plex_music_album.py
+++ plex_music_album.py
@@ -13,6 +13,9 @@ if __name__=='__main__':
                        help = 'Name of the album to get album image for.' )
     parser.add_option( '--songs', dest='do_songs', action='store_true', default = False,
                        help = 'If chosen, get the song listing instead of downloading the album image.')
+    parser.add_option( '--formatted', dest='do_formatted', action='store_true', default = False,
+                       help = ' '.join([ 'If chosen, print the song listing in a format recognized by plex_music_metafill.py'
+                                         'for downloading a collection of songs.' ]) )
     opts, args = parser.parse_args( )
     assert(all(map(lambda tok: tok is not None, ( opts.artist_name, opts.album_name ) ) ) )
     #
@@ -22,5 +25,8 @@ if __name__=='__main__':
         pm.get_song_image( opts.artist_name, opts.album_name )
     else:
         track_listing = pm.get_song_listing( opts.artist_name, album_name = opts.album_name )
-        for title, trkno in track_listing:
-            print( '%02d - %s' % ( trkno, title ) )
+        if not opts.do_formatted:
+            for title, trkno in track_listing:
+                print( '%02d - %s' % ( trkno, title ) )
+        else:
+            print( ';'.join(map(lambda title_trkno: title_trkno[0], track_listing)) )

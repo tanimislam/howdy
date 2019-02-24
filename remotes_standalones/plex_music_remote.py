1d53156e6730108e58c42a60b3b029a8e3383c4f
Tanim Islam <tanim.islam@gmail.com>
2019-02-24 10:56:21 -0800

    now allow plex_music_remotes.py to download albums.

--- remotes_standalones/plex_music_remote.py
+++ remotes_standalones/plex_music_remote.py
@@ -3,6 +3,30 @@
 import codecs, os, sys, base64, requests, time
 from optparse import OptionParser
 
+def get_album( artist, album ):
+    url = 'https://***REMOVED***islam.ddns.net/flask/plex/sendmusic'
+    data = { 'mode' : 'SENDARTISTALBUM',
+             'artist' : artist,
+             'album' : album }
+    response = requests.post( url, json = data,
+                              auth = ( '***REMOVED***_shahriar_islam@yahoo.com',
+                                       'initialplexserver' ) )
+    if response.status_code == 401:
+        print( "ERROR, email '%s' is not known by the Plex Server. Please choose a valid email." % '***REMOVED***_shahriar_islam@yahoo.com' )
+        return
+    elif response.status_code == 400:
+        message = response.json( )['message']
+        print( message )
+        return
+    elif response.status_code == 200:
+        data_dict = response.json( )['data_dict']
+    else:
+        print( 'ERROR, server https://***REMOVED***islam.ddns.net/flask may be down.' )
+        return
+    tracks = data_dict[ 'tracks' ]
+    print( 'ACTUAL NUM TRACKS: %d' % len( tracks ) )
+    for song in tracks: get_song( artist, song )
+    
 def get_song( artist, song ):
     url = 'https://***REMOVED***islam.ddns.net/flask/plex/sendmusic'
     data = { 'mode' : 'SENDARTISTSONG',
@@ -72,10 +96,13 @@ def get_song( artist, song ):
 if __name__=='__main__':
     parser = OptionParser( )
     parser.add_option( '-s', '--song', dest='song_name', type=str, action='store',
-                       help = 'Name of the song to put into the M4A file.' )
+                       help = 'Name of the song.' )
     parser.add_option( '-a', '--artist', dest='artist_name', type=str, action='store',
-                       help = 'Name of the artist to put into the M4A file.' )
+                       help = 'Name of the artist.' )
+    parser.add_option( '-A', '--album', dest='album_name', type=str, action='store',
+                       help = 'Name of the album to download. If defined, then ignore song.' )
     opts, args = parser.parse_args( )
-    assert(all(map(lambda tok: tok is not None,
-                   ( opts.song_name, opts.artist_name ) ) ) )
-    get_song( opts.artist_name, opts.song_name )
+    assert opts.artist_name is not None
+    assert(any(list(map(lambda tok: tok is not None, ( opts.song_name, opts.album_name ) ) ) ) )
+    if opts.album_name is not None: get_album( opts.artist_name, opts.album_name )
+    else: get_song( opts.artist_name, opts.song_name )

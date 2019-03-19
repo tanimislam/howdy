5e10b588e3c20b16a603aa719e1961b28097be4d
Tanim Islam <tanim.islam@gmail.com>
2019-03-19 13:10:54 -0700

    Fixed some stuff in "plexinitialization.py".
    Refactored Gracenote API, LastFM API, and GMusic API code to use configuration database.
    
    Refactored Gracenote configuration into plexmusic.PlexMusic object and class.
    
    Refactor LastFM configuration into plexmusic.PlexLastFM object and class.
    
    Changes to be committed:
    	modified:   plexcore/plexinitialization.py
    	modified:   plexmusic/plexmusic.py
    	modified:   upload_to_gmusic.py

--- upload_to_gmusic.py
+++ upload_to_gmusic.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import signal
+from plexcore import signal_handler
+signal.signal( signal.SIGINT, signal_handler )
 import os, glob
 from optparse import OptionParser
 from plexmusic import plexmusic
@@ -12,13 +15,25 @@ def _files_from_glob(fnames_string):
 
 if __name__=='__main__':
     parser = OptionParser()
-    parser.add_option('--filenames', dest='filenames', action='store', type=str,
+    parser.add_option('-f', '--filenames', dest='filenames', action='store', type=str,
                       help = 'Give the list of filenames to put into the Google Music Player.')
+    parser.add_option('-P', dest='do_push', action='store_true', default = False,
+                      help = 'If chosen, then push Google Music credentials into configuration database.' )
+    parser.add_option('-e', dest='email', action='store', type=str,
+                      help = 'Google Music email.' )
+    parser.add_option('-p', dest='password', action='store', type=str,
+                      help = 'Google Music password.' )
     opts, args = parser.parse_args()
-    if opts.filenames is None:
-        raise ValueError("Error, must give a list of file names.")
-    if '*' in opts.filenames:
-        fnames = _files_from_glob(opts.filenames)
+    if not opts.do_push:
+        if opts.filenames is None:
+            raise ValueError("Error, must give a list of file names.")
+        if '*' in opts.filenames:
+            fnames = _files_from_glob(opts.filenames)
+        else:
+            fnames = _files_from_commas(opts.filenames)
+        plexmusic.upload_to_gmusic(fnames)
     else:
-        fnames = _files_from_commas(opts.filenames)
-    plexmusic.upload_to_gmusic(fnames)
+        if any( map(lambda tok: tok is not None, ( opts.email, opts.password ) ) ):
+            raise ValueError( "Error, must define both Google Music email and password." )
+        plexmusic.save_gmusic_creds( opts.email.strip( ), opts.password.strip( ) )
+        

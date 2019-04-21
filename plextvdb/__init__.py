84ba2e6cdf2735d57b1f819f23c945e7a8525a69
Tanim Islam <tanim.islam@gmail.com>
2019-04-21 13:55:01 -0700

    Please enter the commit message for your changes. Lines starting
    1. improved performance, and changed to work with improved backend, for get_plextvdb_batch.py
    
    2. replaced all configuration databases to use PlexConfig ORM database to store configuration data.
    
    3. removed explicit hard-coding of tvdb and tmdb API keys,
    
     Changes to be committed:
    	modified:   get_plextvdb_batch.py
    	modified:   plexcore/__init__.py
    	modified:   plexcore/plexcore.py
    	modified:   plexcore/plexcore_deluge.py
    	modified:   plexcore/plexcore_rsync.py
    	modified:   plexmusic/__init__.py
    	modified:   plexmusic/plexmusic.py
    	modified:   plextmdb/__init__.py
    	modified:   plextmdb/plextmdb.py
    	modified:   plextvdb/__init__.py
    	modified:   plextvdb/plextvdb.py

--- plextvdb/__init__.py
+++ plextvdb/__init__.py
@@ -3,22 +3,50 @@ import os, requests, json, sys
 from sqlalchemy import Column, String
 mainDir = os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) )
 sys.path.append( mainDir )
-from plexcore import Base, session
+from plexcore import session, create_all, PlexConfig, Base
+from sqlalchemy import String, Column
 
-_apiKey = '0B3F6D72213D71C8'
-_usrKey = 'AEE839E62568BA63'
-_usname = 'tanimislam1978'
+class ShowsToExclude( Base ): # these are shows you want to exclude
+    #
+    ## create the table using Base.metadata.create_all( _engine )
+    __tablename__ = 'showstoexclude'
+    __table_args__ = { 'extend_existing': True }
+    show = Column( String( 65536 ), index = True, primary_key = True )
+
+#
+## commit all tables
+create_all( )
+    
+def save_tvdb_api( username, apikey, userkey ):
+    query = session.query( PlexConfig ).filter( PlexConfig.service == 'tvdb' )
+    val = query.first( )
+    if val is not None:
+        session.delete( val )
+        session.commit( )
+    newval = PlexConfig( service = 'tvdb',
+                         data = {
+                             'apikey' : apikey,
+                             'username' : username,
+                             'userkey' : userkey } )
+    session.add( newval )
+    session.commit( )
+
+def get_tvdb_api( ):
+    query = session.query( PlexConfig ).filter( PlexConfig.service == 'tvdb' )
+    val = query.first( )
+    if val is None:
+        raise ValueError("ERROR, NO TVDB API CREDENTIALS FOUND")
+    data = val.data
+    return { 'username' : data['username'],
+             'apikey' : data['apikey'],
+             'userkey' : data['userkey'] }
 
 def get_token( verify = True ):
-    data = { 'apikey' : _apiKey,
-             'username' : _usname,
-             'userkey' : _usrKey }
     headers = { 'Content-Type' : 'application/json' }
     response = requests.post( 'https://api.thetvdb.com/login',
-                              data = json.dumps( data ),
+                              data = json.dumps( get_tvdb_api( ) ),
                               verify = verify, headers = headers )
-    if response.status_code != 200:
-        return None
+    if response.status_code != 200: return None
     return response.json( )[ 'token' ]
 
 def refresh_token( token, verify = True ):
@@ -29,10 +57,3 @@ def refresh_token( token, verify = True ):
     if response.status_code != 200:
         return None
     return response.json( )['token']
-
-class ShowsToExclude( Base ): # these are shows you want to exclude
-    #
-    ## create the table using Base.metadata.create_all( _engine )
-    __tablename__ = 'showstoexclude'
-    __table_args__ = { 'extend_existing': True }
-    show = Column( String( 65536 ), index = True, primary_key = True )

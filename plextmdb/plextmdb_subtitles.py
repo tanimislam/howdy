1fea2cf4059c29f6110fd0591df4fe15277ef6ff
Tanim Islam <tanim.islam@gmail.com>
2019-05-02 21:07:46 -0700

    1. resources/requirements.txt: added termcolor, needed in get_mov_subtitle.py
    2. get_mov_subtitle.py: fixed functionality, now YTS subtitles work by default.
    
    3. plextmdb/plextmdb_subtitles.py: fixed YTS subtitle downloading.
    
    Changes to be committed:
    	modified:   get_mov_subtitle.py
    	modified:   plextmdb/plextmdb_subtitles.py
    	modified:   resources/requirements.txt

--- plextmdb/plextmdb_subtitles.py
+++ plextmdb/plextmdb_subtitles.py
@@ -1,15 +1,18 @@
-import requests, plextmdb, os, re
+import requests, os, re, logging
 import sys, zipfile, codecs
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 from io import BytesIO
 from plexcore import subscene
+from . import plextmdb
 
 def get_subtitles_opensubtitles( title, extra_strings = [ ] ):
     headers = {'User-Agent': 'TanimIslamSubtitles v1'}
     response = requests.get('https://rest.opensubtitles.org/search/query-%s/sublanguageid-eng' %
                             '%20'.join( title.lower( ).split( ) ), headers = headers )
-    if response.status_code != 200: return None
+    if response.status_code != 200:
+        logging.info( '%d, %s' % ( response.status_code, response.content ) )
+        return None
     subtitles = list(map(lambda item: { 'title'   : item['SubFileName'],
                                         'srtfile' : item['ZipDownloadLink'] },
                          response.json( ) ) )
@@ -20,20 +23,22 @@ def get_subtitles_opensubtitles( title, extra_strings = [ ] ):
     return { item['title'] : item['srtfile'] for item in subtitles }
 
 def get_subtitles_subscene( title, extra_strings = [ ] ):
-    subtitle_tups = subscene.search( title )
-    if subtitle_tups is None: return None
-    if len( extra_strings ) != 0:
-        subtitle_tups = list( filter(lambda url_title:
-                                     any(map(lambda tok: tok.lower( ) in url_title[1].lower( ),
-                                             set( extra_strings) ) ), subtitle_tups ) )
-    if len( subtitle_tups ) == 0:
-        return None
-    subtitles_map = { }
-    for url_title in subtitle_tups:
-        url, title = url_title
-        if title not in subtitles_map:
-            subtitles_map[ title ] = url
-    return subtitles_map
+    try:
+        subtitle_tups = subscene.search( title )
+        if subtitle_tups is None: return None
+        if len( extra_strings ) != 0:
+            subtitle_tups = list( filter(lambda url_title:
+                                         any(map(lambda tok: tok.lower( ) in url_title[1].lower( ),
+                                                 set( extra_strings) ) ), subtitle_tups ) )
+        if len( subtitle_tups ) == 0:
+            return None
+        subtitles_map = { }
+        for url_title in subtitle_tups:
+            url, title = url_title
+            if title not in subtitles_map:
+                subtitles_map[ title ] = url
+        return subtitles_map
+    except: return None
 
 def get_subtitles_yts( title ):
     tmdbid = plextmdb.get_movie_tmdbids( title )
@@ -54,43 +59,42 @@ def get_subtitles_yts( title ):
             return False
         #
         ## language elem
-        lang_elems = filter(lambda elm: 'class' in elm.attrs and max(elm.attrs['class']) == 'sub-lang', elem.find_all('span'))
+        lang_elems = list(filter(lambda elm: 'class' in elm.attrs and
+                                 max(elm.attrs['class']) == 'sub-lang', elem.find_all('span')))
         if len( lang_elems ) != 1:
             return False
         lang_elem = max( lang_elems )
         language = lang_elem.get_text( ).lower( )
         return language == 'english'
 
-    valid_st_elems = filter( is_english_subtitle, html.find_all('tr') )[:5]
+    valid_st_elems = list( filter( is_english_subtitle, html.find_all('tr') ) )[:5]
     if len( valid_st_elems ) == 0:
         return None
     valid_subs = [ ]
     for subtitle_elem in valid_st_elems:
-        first_href = filter(lambda elm: elm.text.strip().startswith('subtitle'), subtitle_elem.find_all('a'))
-        if len( first_href ) != 1:
-            continue
+        first_href = list(
+            filter(lambda elm: elm.text.strip().startswith('subtitle'), subtitle_elem.find_all('a')))
+        if len( first_href ) != 1: continue
         first_href = max( first_href )
         name = ' '.join( first_href.text.strip( ).split( )[1:] )
-        last_href = filter(lambda elm: 'class' in elm.attrs and max(elm.attrs['class']) == 'subtitle-download',
-                           subtitle_elem.find_all('a'))
-        if len( last_href ) != 1:
-            continue
+        last_href = list(
+            filter(lambda elm: 'class' in elm.attrs and max(elm.attrs['class']) == 'subtitle-download',
+                   subtitle_elem.find_all('a')))
+        if len( last_href ) != 1: continue
         last_href = max( last_href )
         pageurl = urljoin( 'http://www.yifysubtitles.com', last_href.attrs['href'] )
         resp2 = requests.get( pageurl )
         if resp2.status_code != 200:
             continue
         html2 = BeautifulSoup( resp2.content, 'lxml' )
-        button_elem = filter(lambda elm: 'href' in elm.attrs and elm.attrs['href'].endswith('.zip'), html2.find_all('a'))
-        if len( button_elem ) != 1:
-            continue
+        button_elem = list(
+            filter(lambda elm: 'href' in elm.attrs and elm.attrs['href'].endswith('.zip'), html2.find_all('a')))
+        if len( button_elem ) != 1: continue
         button_elem = max( button_elem )
         url = button_elem.attrs['href']
-        if not url.endswith('zip'):
-            continue
+        if not url.endswith('zip'): continue
         valid_subs.append({ 'name' : name, 'url' : url })
-    if len( valid_subs ) == 0:
-        return None
+    if len( valid_subs ) == 0: return None
     return valid_subs
 
 def download_yts_sub( url, srtfilename = 'eng.srt' ):

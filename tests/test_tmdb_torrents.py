9be6506448be170c9ced4cf735b11b2d630f156c
Tanim Islam <tanim.islam@gmail.com>
2019-05-05 01:32:09 -0700

    Huge bunch of changes to fix the plex_tmdb_gui functionalities, and to start work on the plex_tvdb_gui functionalities.
    Changes to be committed:
    	modified:   plexemail/plexemail.py
    	modified:   plextmdb/plextmdb_gui.py
    	modified:   plextmdb/plextmdb_mygui.py
    	modified:   plextmdb/plextmdb_torrents.py
    	modified:   plextvdb/plextvdb.py
    	modified:   plextvdb/plextvdb_gui.py
    	deleted:    tests/Big_Hero_6.0.json
    	new file:   tests/Big_Hero_6.json
    	deleted:    tests/East_Side_Sushi.2.json
    	new file:   tests/Star_Trek_VI.json
    	deleted:    tests/The_Witch.1.json
    	new file:   tests/didend_20190504.pkl.gz
    	modified:   tests/test_tmdb_torrents.py
    	new file:   tests/test_tvdb_gui.py
    	new file:   tests/tvdata_20190504.pkl.gz

--- tests/test_tmdb_torrents.py
+++ tests/test_tmdb_torrents.py
@@ -1,13 +1,17 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
-import os, sys, glob
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
-from optparse import OptionParser
+import os, sys, glob, logging, signal
+# code to handle Ctrl+C, convenience method for command line tools
+def signal_handler( signal, frame ):
+    print( "You pressed Ctrl+C. Exiting...")
+    sys.exit( 0 )
+signal.signal( signal.SIGINT, signal_handler )
 mainDir = os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) )
 sys.path.append( mainDir )
+from PyQt4.QtGui import QApplication
+from optparse import OptionParser
 from plextmdb import plextmdb_gui
-from plexcore import plexcore_gui
+from plexcore import plexcore
 
 parser = OptionParser( )
 parser.add_option('--movie', dest='movie', type=str, action='store',
@@ -15,11 +19,17 @@ parser.add_option('--movie', dest='movie', type=str, action='store',
                   ' '.join([ 'Name of movie to do torrent for.',
                              'Default is Big Hero 6.' ]) )
 parser.add_option('--bypass', dest='do_bypass', action='store_true',
-                  default = False, help = 'If chosen, then bypass using Kickass.' )
+                  default = False, help = 'If chosen, then bypass using YTS Movies.' )
+parser.add_option('--debug', dest='do_debug', action='store_true', default = False,
+                  help = 'If chosen, then run with debug mode.' )
 opts, args = parser.parse_args( )
-
+#
 app = QApplication([])
-_, token = plexcore_gui.returnClientToken( )
+app.setStyleSheet(
+    open( os.path.join( mainDir, 'resources', 'ubuntu.qss' ), 'r' ).read( ) )
+if opts.do_debug: logging.basicConfig( level = logging.DEBUG )
+_, token = plexcore.checkServerCredentials( doLocal = True )
 tmdbt = plextmdb_gui.TMDBTorrents( None, token, opts.movie,
-                                   bypass = opts.do_bypass )
+                                   bypass = opts.do_bypass,
+                                   do_debug = opts.do_debug )
 result = tmdbt.exec_( )

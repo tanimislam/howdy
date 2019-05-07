10e8efd480a2311831559782101d3cd597c0f457
Tanim Islam <tanim.islam@gmail.com>
2019-05-07 00:02:41 -0700

    Changes to be committed: 	modified:   plex_tmdb_totgui.py 	modified:   plexcore/plexcore.py 	modified:   plexcore/plexinitialization.py 	modified:   plextmdb/plextmdb.py 	modified:   plextmdb/plextmdb_gui.py 	modified:   plextmdb/plextmdb_mygui.py 	modified:   plextmdb/plextmdb_torrents.py 	modified:   plextmdb/plextmdb_totgui.py 	modified:   resources/requirements.txt 	new file:   tests/movie_data_rows_20190506.pkl.gz 	deleted:    tests/plex_tmdb_gui.py 	deleted:    tests/plex_tmdb_mygui.py 	new file:   tests/test_tmdb_gui.py 	new file:   tests/test_tmdb_mygui.py 	new file:   tests/test_tmdb_totgui.py 	modified:   tests/test_tvdb_gui.py

--- tests/test_tmdb_gui.py
+++ tests/test_tmdb_gui.py
@@ -6,21 +6,24 @@ def signal_handler( signal, frame ):
     print( "You pressed Ctrl+C. Exiting...")
     sys.exit( 0 )
 signal.signal( signal.SIGINT, signal_handler )
-import logging, PyQt4.QtGui, os, qdarkstyle
-from plexcore import plexcore
-from plextmdb import plextmdb_totgui
+import logging, PyQt4.QtGui, sys, pickle, gzip, os, qdarkstyle
+mainDir = os.path.dirname(
+    os.path.dirname( os.path.abspath( __file__ ) ) )
+sys.path.append( mainDir )
 from optparse import OptionParser
+from plexcore import plexcore
+from plextmdb import plextmdb_gui
 
-def main(debug = False, doLocal = True, doLarge = False, verify = True):
+def main(debug = False, doLocal = True, verify = True ):
     app = PyQt4.QtGui.QApplication([])
     app.setStyleSheet( qdarkstyle.load_stylesheet_pyqt( ) )
     if debug: logging.basicConfig( level = logging.DEBUG )
     fullurl, token = plexcore.checkServerCredentials(
-        doLocal = doLocal, verify = verify )    
-    tmdb_mygui = plextmdb_totgui.TMDBTotGUI( fullurl, token, doLarge = doLarge,
-                                             verify = verify )
+        doLocal = doLocal, verify = verify )
+    movie_data_rows = pickle.load( gzip.open( 'movie_data_rows_20190506.pkl.gz', 'rb' ) )
+    tmdbgui = plextmdb_gui.TMDBGUI( token, fullurl, movie_data_rows, verify = verify )
     result = app.exec_( )
-    return tmdb_mygui
+    return tmdbgui
 
 if __name__=='__main__':
     parser = OptionParser( )
@@ -29,6 +32,6 @@ if __name__=='__main__':
     parser.add_option('--debug', dest='do_debug', action='store_true',
                       default = False, help = 'Run debug mode if chosen.')
     parser.add_option('--noverify', dest='do_verify', action='store_false',
-                      default = True, help = 'Do not verify SSL transactions if chosen.')    
+                      default = True, help = 'Do not verify SSL transactions if chosen.')
     opts, args = parser.parse_args( )
     main( debug = opts.do_debug, doLocal = opts.do_local, verify = opts.do_verify )

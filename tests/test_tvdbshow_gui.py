f6b0db7a51e7a77917f8ba6b79a846872b6f13a6
Tanim Islam <tanim.islam@gmail.com>
2019-05-09 02:35:35 -0700

    Changes to be committed: 	modified:   .gitignore 	modified:   plexcore/plexcore.py 	modified:   plextvdb/plextvdb.py 	modified:   plextvdb/plextvdb_gui.py 	new file:   plextvdb/plextvdb_season_gui.py 	new file:   tests/test_qstackedwidget.py 	modified:   tests/test_tvdb_gui.py 	new file:   tests/test_tvdbseason_gui.py 	new file:   tests/test_tvdbshow_gui.py 	modified:   tests/tvshow_dict_20190507.pkl.gz

--- tests/test_tvdbshow_gui.py
+++ tests/test_tvdbshow_gui.py
@@ -9,8 +9,7 @@ signal.signal( signal.SIGINT, signal_handler )
 import qdarkstyle, pickle, gzip
 mainDir = os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) )
 sys.path.append( mainDir )
-mainDir = os.path.dirname( os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) ) )
-sys.path.append( mainDir )
+sys.path.append( os.path.dirname( mainDir ) )
 from PyQt4.QtGui import QApplication
 from plextvdb import plextvdb_gui
 from plexcore import plexcore
@@ -19,11 +18,8 @@ from plexcore import plexcore
 ## start the application here
 app = QApplication([])
 app.setStyleSheet( qdarkstyle.load_stylesheet_pyqt( ) )
-fullURL, token = plexcore.checkServerCredentials(
-    doLocal = False, verify = False )
-tvdbg = plextvdb_gui.TVDBGUI(
-    token, fullURL, tvdata_on_plex = pickle.load(
-        gzip.open('tvdata_20190504.pkl.gz', 'rb' ) ),
-    tvshow_dict = pickle.load( gzip.open( 'tvshow_dict_20190507.pkl.gz', 'rb' ) ),
-    verify = False )
+tvdata = pickle.load( gzip.open('tvdata_20190504.pkl.gz', 'rb' ) )
+tvshow_dict = pickle.load( gzip.open( 'tvshow_dict_20190507.pkl.gz', 'rb' ) )
+tvdb_show_gui = plextvdb_gui.TVDBShowGUI(
+    "Bob's Burgers", tvdata, tvshow_dict, verify = False)
 result = app.exec_( )

080d7f7039da45d29fc8a98769c3ac88b342da1b
Tanim Islam <tanim.islam@gmail.com>
2019-05-12 01:46:51 -0700

    Changes to be committed: 	modified:   plextvdb/plextvdb.py 	modified:   plextvdb/plextvdb_gui.py 	modified:   plextvdb/plextvdb_season_gui.py 	deleted:    tests/didend_20190511.pkl.gz 	new file:   tests/didend_20190512.pkl.gz 	deleted:    tests/episodes.pkl.gz 	modified:   tests/test_tvdb_gui.py 	modified:   tests/test_tvdbshow_gui.py 	deleted:    tests/toGet_20190511.pkl.gz 	new file:   tests/toGet_20190512.pkl.gz 	renamed:    tests/tvdata_20190511.pkl.gz -> tests/tvdata_20190512.pkl.gz

--- tests/test_tvdbshow_gui.py
+++ tests/test_tvdbshow_gui.py
@@ -11,15 +11,18 @@ mainDir = os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) )
 sys.path.append( mainDir )
 sys.path.append( os.path.dirname( mainDir ) )
 from PyQt4.QtGui import QApplication
-from plextvdb import plextvdb_gui
+from plextvdb import plextvdb_gui, get_token
 from plexcore import plexcore
 
 #
 ## start the application here
 app = QApplication([])
 app.setStyleSheet( qdarkstyle.load_stylesheet_pyqt( ) )
-tvdata = pickle.load( gzip.open('tvdata_20190504.pkl.gz', 'rb' ) )
-tvshow_dict = pickle.load( gzip.open( 'tvshow_dict_20190507.pkl.gz', 'rb' ) )
+tvdata = pickle.load( gzip.open('tvdata_20190512.pkl.gz', 'rb' ) )
+toGet = pickle.load( gzip.open( 'toGet_20190512.pkl.gz', 'rb' ) )
+_, plex_token = plexcore.checkServerCredentials( doLocal = False )
+tvdb_token = get_token( )
 tvdb_show_gui = plextvdb_gui.TVDBShowGUI(
-    "Bob's Burgers", tvdata, tvshow_dict, verify = False)
-result = app.exec_( )
+    "Bob's Burgers", tvdata, toGet, tvdb_token, plex_token,
+    verify = True)
+result = tvdb_show_gui.exec_( )

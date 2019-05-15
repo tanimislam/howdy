f6a4a4035ec56619dcc672f3a5bfa803550b822f
Tanim Islam <islam5@llnl.gov>
2019-05-14 21:15:29 -0700

    Started small: adding python docstrings to plextmdb/__init__.py
    Changes to be committed:
    	modified:   plextmdb/__init__.py

--- plextmdb/__init__.py
+++ plextmdb/__init__.py
@@ -5,6 +5,20 @@ sys.path.append( mainDir )
 from plexcore import session, PlexConfig
 
 def save_tmdb_api( apikey ):
+    """
+    Saves the provided TMDB API key into the database, stored on disk at ~/.config/plexstuff/app.db.
+
+    Parameters
+    ----------
+    apikey (string): The TMDB API string, whose format is described in `The TMDB API`_.
+
+    Returns
+    -------
+    None
+
+    .._The TMDB API:
+       https://developers.themoviedb.org/3/getting-started/introduction
+    """
     query = session.query( PlexConfig ).filter( PlexConfig.service == 'tmdb' )
     val = query.first( )
     if val is not None:
@@ -16,11 +30,15 @@ def save_tmdb_api( apikey ):
     session.commit( )
 
 def get_tmdb_api( ):
+    """
+    Returns the TMDB API key found in the database, stored on disk at ~/.config/plexstuff/app.dbself.
+
+    Returns
+        string: the TMDB API key stored in the database.
+    -------
+    """
     query = session.query( PlexConfig ).filter( PlexConfig.service == 'tmdb' )
     val = query.first( )
     if val is None:
         raise ValueError("ERROR, NO TMDB API CREDENTIALS FOUND")
     return val.data['apikey']
-    
-
-

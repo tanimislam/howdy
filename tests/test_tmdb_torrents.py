d6b68bfa8de0dc71b67e2273ee14b16d2ed73c35
Tanim Islam <tanim.islam@gmail.com>
2019-05-16 02:47:50 -0700

    Changes to be committed: 	modified:   plextmdb/plextmdb.py 	modified:   plextmdb/plextmdb_gui.py 	new file:   tests/conftest.py 	modified:   tests/test_plexcore.py 	new file:   tests/test_tmdb.py 	modified:   tests/test_tmdb_torrents.py 	modified:   tests/test_tmdbgui.py 	modified:   tests/test_tvdb_gui.py 	modified:   tests/test_tvdbgui.py

--- tests/test_tmdb_torrents.py
+++ tests/test_tmdb_torrents.py
@@ -15,14 +15,13 @@ parser.add_option('--movie', dest='movie', type=str, action='store',
                              'Default is Big Hero 6.' ]) )
 parser.add_option('--bypass', dest='do_bypass', action='store_true',
                   default = False, help = 'If chosen, then bypass using YTS Movies.' )
-parser.add_option('--debug', dest='do_debug', action='store_true', default = False,
-                  help = 'If chosen, then run with debug mode.' )
+parser.add_option('--info', dest='do_info', action='store_true', default = False,
+                  help = 'If chosen, then run with info mode.' )
 opts, args = parser.parse_args( )
 #
 app = test_tmdbgui.get_app_standalone( )
-if opts.do_debug: logging.basicConfig( level = logging.DEBUG )
+if opts.do_info: logging.basicConfig( level = logging.INFO )
 _, token = test_plexcore.get_token_fullURL_standalone( )
 tmdbt = plextmdb_gui.TMDBTorrents( None, token, opts.movie,
-                                   bypass = opts.do_bypass,
-                                   do_debug = opts.do_debug )
+                                   bypass = opts.do_bypass )
 result = tmdbt.exec_( )

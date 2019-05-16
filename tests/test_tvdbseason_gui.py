f56434d8685a7d11031f6c516c9645e24393530e
Tanim Islam <tanim.islam@gmail.com>
2019-05-16 00:42:25 -0700

    Changes to be committed: 	modified:   plexcore/__init__.py 	modified:   plexcore/plexcore.py 	modified:   plextmdb/__init__.py 	modified:   plextmdb/plextmdb.py 	modified:   plextvdb/plextvdb_gui.py 	new file:   tests/__init__.py 	new file:   tests/didend.pkl.gz 	deleted:    tests/didend_20190513.pkl.gz 	renamed:    tests/movie_data_rows_20190511.pkl.gz -> tests/movie_data_rows.pkl.gz 	new file:   tests/test_plexcore.py 	new file:   tests/test_plexinitialization.py 	deleted:    tests/test_qstackedwidget.py 	modified:   tests/test_tmdb_gui.py 	modified:   tests/test_tmdb_mygui.py 	modified:   tests/test_tmdb_torrents.py 	modified:   tests/test_tmdb_totgui.py 	new file:   tests/test_tmdbgui.py 	modified:   tests/test_tvdb_gui.py 	new file:   tests/test_tvdbgui.py 	modified:   tests/test_tvdbseason_gui.py 	modified:   tests/test_tvdbshow_gui.py 	new file:   tests/toGet.pkl.gz 	deleted:    tests/toGet_20190513.pkl.gz 	new file:   tests/tvdata.pkl.gz 	deleted:    tests/tvdata_20190513.pkl.gz

--- tests/test_tvdbseason_gui.py
+++ tests/test_tvdbseason_gui.py
@@ -1,42 +1,33 @@
 #!/usr/bin/env python3
 
-import os, sys, glob, logging, signal
-# code to handle Ctrl+C, convenience method for command line tools
-def signal_handler( signal, frame ):
-    print( "You pressed Ctrl+C. Exiting...")
-    sys.exit( 0 )
+import signal
+from . import signal_handler
 signal.signal( signal.SIGINT, signal_handler )
-import qdarkstyle, pickle, gzip
-mainDir = os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) )
-sys.path.append( mainDir )
-sys.path.append( os.path.dirname( mainDir ) )
-from PyQt4.QtGui import QApplication
 from plextvdb import plextvdb_season_gui, get_token
 from plexcore import plexcore
 from optparse import OptionParser
+import test_plexcore, test_tvdbgui
 
 #
 ## start the application here
-logging.basicConfig( level = logging.INFO )
-parser = OptionParser( )
-parser.add_option('-s', '--series', type=str, dest='series', action='store', default='The Simpsons',
-                  help = 'Name of the series to choose. Default is "The Simpsons".' )
-parser.add_option('-S', '--season', type=int, dest='season', action='store', default=1,
-                  help = 'Season number to examine. Default is 1.' )
-opts, args = parser.parse_args( )
-app = QApplication([])
-app.setStyleSheet( qdarkstyle.load_stylesheet_pyqt( ) )
-tvdata = pickle.load( gzip.open(max(glob.glob('tvdata_*pkl.gz')), 'rb' ))
-toGet = pickle.load( gzip.open(max(glob.glob('toGet_*pkl.gz')), 'rb' ))
-assert( opts.season > 0 )
-assert( opts.series in tvdata )
-missing_eps = dict(map(
-    lambda seriesName: ( seriesName, toGet[ seriesName ][ 'episodes' ] ),
-    toGet ) )#
-fullURL, plex_token = plexcore.checkServerCredentials(
-    doLocal = False, verify = False )
-logging.info('got here')
-tvdb_season_gui = plextvdb_season_gui.TVDBSeasonGUI(
-    opts.series, opts.season, tvdata, missing_eps, get_token( False ),
-    plex_token, verify = False )
-result = app.exec_( )
+if __name__=='__main__':
+    logging.basicConfig( level = logging.INFO )
+    parser = OptionParser( )
+    parser.add_option('-s', '--series', type=str, dest='series', action='store', default='The Simpsons',
+                      help = 'Name of the series to choose. Default is "The Simpsons".' )
+    parser.add_option('-S', '--season', type=int, dest='season', action='store', default=1,
+                      help = 'Season number to examine. Default is 1.' )
+    opts, args = parser.parse_args( )
+    app = test_tvdbgui.get_app_standalone( )
+    tvdata, toGet, _ = test_tvdbgui.get_tvdata_toGet_didend_standalone( )
+    fullURL, token = test_plexcore.get_token_fullURL_standalone( )
+    assert( opts.season > 0 )
+    assert( opts.series in tvdata )
+    assert( opts.season in tvdata[ opts.series ][ 'seasons' ] )
+    missing_eps = dict(map(
+        lambda seriesName: ( seriesName, toGet[ seriesName ][ 'episodes' ] ),
+        toGet ) )#
+    tvdb_season_gui = plextvdb_season_gui.TVDBSeasonGUI(
+        opts.series, opts.season, tvdata, missing_eps, get_token( True ),
+        plex_token, verify = True )
+    result = app.exec_( )

cf2b3ec5340b4c3081cf4627560ca07450da0eae
Tanim Islam <tanim.islam@gmail.com>
2019-05-16 02:47:50 -0700

    Changes to be committed: 	modified:   plextmdb/plextmdb.py 	modified:   plextmdb/plextmdb_gui.py 	new file:   tests/conftest.py 	modified:   tests/test_plexcore.py 	new file:   tests/test_tmdb.py 	modified:   tests/test_tmdb_torrents.py 	modified:   tests/test_tmdbgui.py 	modified:   tests/test_tvdb_gui.py 	modified:   tests/test_tvdbgui.py

--- tests/test_tvdb_gui.py
+++ tests/test_tvdb_gui.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
 
 import os, sys, glob, logging, signal
-# code to handle Ctrl+C, convenience method for command line tools
-def signal_handler( signal, frame ):
-    print( "You pressed Ctrl+C. Exiting...")
-    sys.exit( 0 )
+from . import signal_handler
 signal.signal( signal.SIGINT, signal_handler )
-from functools import reduce
-mainDir = reduce(lambda x,y: os.path.dirname( x ), range(2),
-                 os.path.abspath( __file__ ) )
-sys.path.append( mainDir )
 import qdarkstyle, pickle, gzip
 from PyQt4.QtGui import QApplication
 from plextvdb import plextvdb_gui
@@ -21,9 +14,9 @@ app = QApplication([])
 app.setStyleSheet( qdarkstyle.load_stylesheet_pyqt( ) )
 fullURL, token = plexcore.checkServerCredentials(
     doLocal = False, verify = False )
-tvdata = pickle.load( gzip.open(max(glob.glob('tvdata_*pkl.gz')), 'rb' ))
-toGet = pickle.load( gzip.open(max(glob.glob('toGet_*pkl.gz')), 'rb' ))
-didend= pickle.load( gzip.open(max(glob.glob('didend_*pkl.gz')), 'rb'))
+tvdata = pickle.load( gzip.open(max(glob.glob('tvdata*pkl.gz')), 'rb' ))
+toGet = pickle.load( gzip.open(max(glob.glob('toGet*pkl.gz')), 'rb' ))
+didend= pickle.load( gzip.open(max(glob.glob('didend*pkl.gz')), 'rb'))
 tvdbg = plextvdb_gui.TVDBGUI(
     token, fullURL, tvdata_on_plex = tvdata,
     toGet = toGet, didend = didend, verify = True )

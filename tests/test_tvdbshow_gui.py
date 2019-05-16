63d013c1b167bc3a43d5aee325c978f6206c6204
Tanim Islam <tanim.islam@gmail.com>
2019-05-16 03:13:03 -0700

    Changes to be committed: 	modified:   plextmdb/plextmdb_gui.py 	modified:   plextmdb/plextmdb_torrents.py 	modified:   plextvdb/plextvdb_gui.py 	modified:   tests/test_tmdb_torrents.py 	modified:   tests/test_tvdb_gui.py 	modified:   tests/test_tvdbseason_gui.py 	modified:   tests/test_tvdbshow_gui.py

--- tests/test_tvdbshow_gui.py
+++ tests/test_tvdbshow_gui.py
@@ -15,23 +15,31 @@ from PyQt4.QtGui import QApplication
 from plextvdb import plextvdb_gui, plextvdb_season_gui, get_token
 from plexcore import plexcore
 from test_plexcore import get_token_fullURL
+from optparse import OptionParser
 
 #
 ## start the application here
 logging.basicConfig( level = logging.INFO )
 parser = OptionParser( )
-parser.add_option('-s', '--series', type=str, dest='series', action='store', default='The Simpsons',
+parser.add_option('-s', '--series', type=str, dest='series', action='store',
+                  default='The Simpsons',
                   help = 'Name of the series to choose. Default is "The Simpsons".' )
 opts, args = parser.parse_args( )
 app = QApplication([])
 app.setStyleSheet( qdarkstyle.load_stylesheet_pyqt( ) )
-tvdata = pickle.load( gzip.open(max(glob.glob('tvdata_*pkl.gz')), 'rb' ))
-toGet = pickle.load( gzip.open(max(glob.glob('toGet_*pkl.gz')), 'rb' ))
-didend= pickle.load( gzip.open(max(glob.glob('didend_*pkl.gz')), 'rb'))
+tvdata = pickle.load( gzip.open(max(glob.glob('tvdata*pkl.gz')), 'rb' ))
+toGet = pickle.load( gzip.open(max(glob.glob('toGet*pkl.gz')), 'rb' ))
+didend= pickle.load( gzip.open(max(glob.glob('didend*pkl.gz')), 'rb'))
 assert( opts.series in tvdata )
-_, plex_token = plexcore.checkServerCredentials( doLocal = False, verify = False )
+_, plex_token = plexcore.checkServerCredentials(
+    doLocal = False, verify = False )
 tvdb_token = get_token( verify = False )
 tvdb_show_gui = plextvdb_gui.TVDBShowGUI(
     opts.series, tvdata, toGet, tvdb_token, plex_token,
     verify = False )
+tvdb_show_gui.setStyleSheet("""
+QWidget {
+font-family: Consolas;
+font-size: 11;
+}""" )
 result = tvdb_show_gui.exec_( )

6f8b83b09d570ddd2cf05c49a5a76e4d67e303d9
Tanim Islam <tanim.islam@gmail.com>
2019-05-16 03:13:03 -0700

    Changes to be committed: 	modified:   plextmdb/plextmdb_gui.py 	modified:   plextmdb/plextmdb_torrents.py 	modified:   plextvdb/plextvdb_gui.py 	modified:   tests/test_tmdb_torrents.py 	modified:   tests/test_tvdb_gui.py 	modified:   tests/test_tvdbseason_gui.py 	modified:   tests/test_tvdbshow_gui.py

--- tests/test_tvdbseason_gui.py
+++ tests/test_tvdbseason_gui.py
@@ -1,33 +1,41 @@
 #!/usr/bin/env python3
 
-import signal
-from . import signal_handler
-signal.signal( signal.SIGINT, signal_handler )
+import signal, sys, os, logging, glob
+def signal_handler( signal, frame ):
+    print( "You pressed Ctrl+C. Exiting...")
+    sys.exit( 0 )
+from functools import reduce
+mainDir = reduce(lambda x,y: os.path.dirname( x ), range(2),
+                 os.path.abspath( __file__ ) )
+sys.path.append( mainDir )
+from PyQt4.QtGui import QApplication
+import qdarkstyle, pickle, gzip
 from plextvdb import plextvdb_season_gui, get_token
 from plexcore import plexcore
 from optparse import OptionParser
-import test_plexcore, test_tvdbgui
 
 #
 ## start the application here
-if __name__=='__main__':
-    logging.basicConfig( level = logging.INFO )
-    parser = OptionParser( )
-    parser.add_option('-s', '--series', type=str, dest='series', action='store', default='The Simpsons',
-                      help = 'Name of the series to choose. Default is "The Simpsons".' )
-    parser.add_option('-S', '--season', type=int, dest='season', action='store', default=1,
-                      help = 'Season number to examine. Default is 1.' )
-    opts, args = parser.parse_args( )
-    app = test_tvdbgui.get_app_standalone( )
-    tvdata, toGet, _ = test_tvdbgui.get_tvdata_toGet_didend_standalone( )
-    fullURL, token = test_plexcore.get_token_fullURL_standalone( )
-    assert( opts.season > 0 )
-    assert( opts.series in tvdata )
-    assert( opts.season in tvdata[ opts.series ][ 'seasons' ] )
-    missing_eps = dict(map(
-        lambda seriesName: ( seriesName, toGet[ seriesName ][ 'episodes' ] ),
-        toGet ) )#
-    tvdb_season_gui = plextvdb_season_gui.TVDBSeasonGUI(
-        opts.series, opts.season, tvdata, missing_eps, get_token( True ),
-        plex_token, verify = True )
-    result = app.exec_( )
+logging.basicConfig( level = logging.INFO )
+parser = OptionParser( )
+parser.add_option('-s', '--series', type=str, dest='series', action='store', default='The Simpsons',
+                  help = 'Name of the series to choose. Default is "The Simpsons".' )
+parser.add_option('-S', '--season', type=int, dest='season', action='store', default=1,
+                  help = 'Season number to examine. Default is 1.' )
+opts, args = parser.parse_args( )
+app = QApplication([])
+app.setStyleSheet( qdarkstyle.load_stylesheet_pyqt( ) )
+tvdata = pickle.load( gzip.open( max(glob.glob('tvdata*pkl.gz')), 'rb'))
+toGet = pickle.load( gzip.open( max(glob.glob('toGet*pkl.gz')), 'rb'))
+fullURL, plex_token = plexcore.checkServerCredentials(
+    doLocal = False, verify = False )
+assert( opts.season > 0 )
+assert( opts.series in tvdata )
+assert( opts.season in tvdata[ opts.series ][ 'seasons' ] )
+missing_eps = dict(map(
+    lambda seriesName: ( seriesName, toGet[ seriesName ][ 'episodes' ] ),
+    toGet ) )#
+tvdb_season_gui = plextvdb_season_gui.TVDBSeasonGUI(
+    opts.series, opts.season, tvdata, missing_eps, get_token( False ),
+    plex_token, verify = False )
+result = app.exec_( )

392791d7f5b06826941de9f89ceda7bf3a4a5e3c
Tanim Islam <tanim.islam@gmail.com>
2019-05-18 23:53:18 -0700

    Changes to be committed: 	modified:   get_mov_tor.py 	modified:   get_tv_tor.py 	modified:   tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,4 @@
-import os, sys, signal
-# code to handle Ctrl+C, convenience method for command line tools
-def signal_handler( signal, frame ):
-    print( "You pressed Ctrl+C. Exiting...")
-    sys.exit( 0 )
-
+import os, sys
 from functools import reduce
 mainDir = reduce(lambda x,y: os.path.dirname( x ), range(2),
                  os.path.abspath( __file__ ) )

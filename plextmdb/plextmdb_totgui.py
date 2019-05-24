85c8ecc0596c3ec0e962f670f644bfcb6834670e
Tanim Islam <tanim.islam@gmail.com>
2019-05-24 02:11:30 -0700

    Changes to be committed: 	modified:   plex_tmdb_totgui.py 	modified:   plexcore/__init__.py 	modified:   plexcore/plexinitialization.py 	modified:   plextmdb/plextmdb_totgui.py 	modified:   plextvdb/plextvdb_gui.py 	new file:   resources/UbuntuMono-B.ttf 	new file:   resources/UbuntuMono-BI.ttf 	new file:   resources/UbuntuMono-R.ttf 	new file:   resources/UbuntuMono-RI.ttf

--- plextmdb/plextmdb_totgui.py
+++ plextmdb/plextmdb_totgui.py
@@ -4,7 +4,7 @@ from . import plextmdb, plextmdb_mygui, plextmdb_gui
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from plexcore import plexcore, plexcore_gui
-
+    
 class TMDBTotGUI( QDialog ):
     emitNewToken = pyqtSignal( str )
         
@@ -18,50 +18,58 @@ class TMDBTotGUI( QDialog ):
             fname = '%s.png' % fname
         qpm = QPixmap.grabWidget( self )
         qpm.save( fname )
-    
-    def __init__( self, fullurl, token, movie_data_rows = None,
-                  doLarge = False, verify = True ):
-        time0 = time.time( )
-        super( TMDBTotGUI, self ).__init__( )
-        self.resolution = 1.0
-        self.verify = verify
-        if doLarge: self.resolution = 2.0
-        self.setWindowTitle('PLEX MOVIE GUI')
-        tmdbEngine = plextmdb.TMDBEngine( verify = verify )
-        self.fullurl = fullurl
-        self.token = token
-        self.setStyleSheet("""
-        QWidget {
-        font-family: Consolas;
-        font-size: %d;
-        }""" % ( int( 11 * self.resolution ) ) )
+
+    class TMDBTotGUIThread( QThread ):
+
+        movieDataRowsSignal = pyqtSignal( list )
+        emitString = pyqtSignal( str )
+
+        def __init__( self, fullURL, token, movie_data_rows = None,
+                      verify = False, time0 = -1 ):
+            super( QThread, self ).__init__( )
+            self.fullURL = fullURL
+            self.token = token
+            self.verify = verify
+            self.movie_data_rows = movie_data_rows
+            self.time0 = time0
+
+        def run( self ):
+            if self.movie_data_rows is None:
+                self.movie_data_rows, _ = plexcore.fill_out_movies_stuff(
+                    self.fullURL, self.token, verify = self.verify )
+            mystr = 'processed existing movie data in %0.3f seconds.' % (
+                time.time( ) - self.time0 )
+            logging.info( mystr )
+            self.emitString.emit( mystr )
+            time.sleep( 0.5 )
+            self.movieDataRowsSignal.emit( self.movie_data_rows )
+
+    def _process_movie_data_rows_init( self, movie_data_rows ):
         #
-        if movie_data_rows is None:
-            movie_data_rows, _ = plexcore.fill_out_movies_stuff(
-                self.fullurl, self.token, verify = self.verify )
-        logging.info( 'processed existing movie data in %0.3f seconds.' % (
-            time.time( ) - time0 ) )
+        ## now change everything
+        self.hide( )
+        self.setWindowTitle( 'PLEX MOVIE GUI' )
+        #
+        ## delete all widgets in current layout
+        while self.myLayout.count( ):
+            myWidget = self.myLayout.takeAt( 0 ).widget( )
+            myWidget.deleteLater( )
+        #
         self.tmdb_gui = plextmdb_gui.TMDBGUI(
             self.token, self.fullurl, movie_data_rows, isIsolated = False,
-            verify = verify )
+            verify = self.verify )
         self.tmdb_mygui = plextmdb_mygui.TMDBMyGUI(
             self.token, movie_data_rows, isIsolated = False,
-            verify = verify )
-        self.tmdb_gui.movieRefreshRows.connect( self.tmdb_mygui.fill_out_movies )
-        self.helpDialog = HelpDialog( self )
-        self.statusDialog = QLabel( )
-        self.tabWidget = QTabWidget( self )
-        self.tabWidget.setStyleSheet( 'QTabBar::tab { width: 340px; }' )
+            verify = self.verify )
+        self.tmdb_gui.movieRefreshRows.connect(
+            self.tmdb_mygui.fill_out_movies )
         #
-        mainLayout = QVBoxLayout( )
-        self.setLayout( mainLayout )
-        #
-        self.tabWidget.addTab( self.tmdb_gui,
-                               'The List of Movies By Genre and Year' )
-        self.tabWidget.addTab( self.tmdb_mygui,
-                               'My Own Movies' )
-        mainLayout.addWidget( self.tabWidget )
-        mainLayout.addWidget( self.statusDialog )
+        self.tabWidget.addTab(
+            self.tmdb_gui, 'The List of Movies By Genre and Year' )
+        self.tabWidget.addTab(
+            self.tmdb_mygui, 'My Own Movies' )
+        self.myLayout.addWidget( self.tabWidget )
+        self.myLayout.addWidget( self.statusDialog )
         #
         printAction = QAction( self )
         printAction.setShortcut( 'Ctrl+P' )
@@ -78,7 +86,59 @@ class TMDBTotGUI( QDialog ):
         qfm = QFontMetrics( qf )        
         #self.setFixedWidth( 150 * qfm.width( 'A' ) )
         self.setFixedWidth( 700 )
+        self.setFixedHeight( self.sizeHint( ).height( ) )
         self.show( )
+
+    def _primordial_put_strings( self, extra_string ):
+        self.primordialErrorStrings.append( extra_string )
+        myHTML = """
+        <html>
+          %s
+        </html>""" % '\n'.join(map(lambda estring: '<p>%s</p>' % estring,
+                                   self.primordialErrorStrings ) )
+        self.primordialErrorDialog.setHtml( myHTML )
+        
+    def __init__( self, fullurl, token, movie_data_rows = None,
+                  doLarge = False, verify = True ):
+        time0 = time.time( )
+        super( TMDBTotGUI, self ).__init__( )
+        self.verify = verify
+        self.resolution = 1.0
+        if doLarge: self.resolution = 2.0
+        self.setWindowTitle( 'PLEX MOVIE GUI PROGRESS WINDOW' )
+        tmdbEngine = plextmdb.TMDBEngine( verify = verify )
+        self.fullurl = fullurl
+        self.token = token
+        self.setStyleSheet("""
+        QWidget {
+        font-family: Consolas;
+        font-size: %d;
+        }""" % ( int( 11 * self.resolution ) ) )
+        #
+        self.helpDialog = HelpDialog( self )
+        self.statusDialog = QLabel( )
+        self.tabWidget = QTabWidget( self )
+        self.tabWidget.setStyleSheet( 'QTabBar::tab { width: 340px; }' )
+        #
+        self.setFixedWidth( 300 )
+        self.setFixedHeight( 300 )
+        self.myLayout = QVBoxLayout( )
+        self.setLayout( self.myLayout )
+        self.primordialErrorDialog = QTextEdit( )
+        self.primordialErrorDialog.setReadOnly( True )
+        self.primordialErrorStrings = [ ]
+        self.myLayout.addWidget( self.primordialErrorDialog )
+        self.show( )
+        #
+        ## have this happen in background, hope it works without crashing...
+        self.mainInitThread = TMDBTotGUI.TMDBTotGUIThread(
+            fullurl, token, movie_data_rows = movie_data_rows,
+            verify = self.verify, time0 = time0 )
+        self.mainInitThread.movieDataRowsSignal.connect(
+            self._process_movie_data_rows_init )
+        self.mainInitThread.emitString.connect(
+            self._primordial_put_strings )
+        self.mainInitThread.start( ) # make the magic start...
 
     def _setupActions( self ):
         quitAction = QAction( self )

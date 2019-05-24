b6f672bf8997e9f470f3420746d6b3fde8a051d3
Tanim Islam <tanim.islam@gmail.com>
2019-05-24 15:58:53 -0700

    Changes to be committed: 	modified:   plexcore/__init__.py 	modified:   plexcore/plexcore.py 	modified:   plextmdb/plextmdb_totgui.py 	modified:   plextvdb/plextvdb_gui.py

--- plextmdb/plextmdb_totgui.py
+++ plextmdb/plextmdb_totgui.py
@@ -4,54 +4,51 @@ from . import plextmdb, plextmdb_mygui, plextmdb_gui
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from plexcore import plexcore, plexcore_gui
-from plexcore import QDialogWithPrinting
-
+from plexcore import QDialogWithPrinting, ProgressDialog
+    
+class TMDBTotGUIThread( QThread ):
+    
+    movieDataRowsSignal = pyqtSignal( list )
+    emitString = pyqtSignal( str )
+    
+    def __init__( self, fullURL, token, movie_data_rows = None,
+                  verify = False, time0 = -1 ):
+        super( QThread, self ).__init__( )
+        self.fullURL = fullURL
+        self.token = token
+        self.verify = verify
+        self.movie_data_rows = movie_data_rows
+        self.time0 = time0
+        
+    def run( self ):
+        mystr = 'started loading in movie data on %s.' % (
+            datetime.datetime.now( ).strftime( '%B %d, %Y @ %I:%M:%S %p' ) )
+        logging.info( mystr )
+        self.emitString.emit( mystr )
+        if self.movie_data_rows is None:
+            self.movie_data_rows, _ = plexcore.fill_out_movies_stuff(
+                self.fullURL, self.token, verify = self.verify )
+        mystr = 'processed existing movie data in %0.3f seconds.' % (
+            time.time( ) - self.time0 )
+        logging.info( mystr )
+        self.emitString.emit( mystr )
+        time.sleep( 0.5 )
+        mystr = 'processed all movie data on %s.' % (
+            datetime.datetime.now( ).strftime( '%B %d, %Y @ %I:%M:%S %p' ) )
+        logging.info( mystr )
+        self.emitString.emit( mystr )
+        self.movieDataRowsSignal.emit( self.movie_data_rows )
+        
 class TMDBTotGUI( QDialogWithPrinting ):
     emitNewToken = pyqtSignal( str )
-    
-    class TMDBTotGUIThread( QThread ):
 
-        movieDataRowsSignal = pyqtSignal( list )
-        emitString = pyqtSignal( str )
-
-        def __init__( self, fullURL, token, movie_data_rows = None,
-                      verify = False, time0 = -1 ):
-            super( QThread, self ).__init__( )
-            self.fullURL = fullURL
-            self.token = token
-            self.verify = verify
-            self.movie_data_rows = movie_data_rows
-            self.time0 = time0
-
-        def run( self ):
-            mystr = 'started loading in movie data on %s.' % (
-                datetime.datetime.now( ).strftime( '%B %d, %Y @ %I:%M:%S %p' ) )
-            logging.info( mystr )
-            self.emitString.emit( mystr )
-            if self.movie_data_rows is None:
-                self.movie_data_rows, _ = plexcore.fill_out_movies_stuff(
-                    self.fullURL, self.token, verify = self.verify )
-            mystr = 'processed existing movie data in %0.3f seconds.' % (
-                time.time( ) - self.time0 )
-            logging.info( mystr )
-            self.emitString.emit( mystr )
-            time.sleep( 0.5 )
-            mystr = 'processed all movie data on %s.' % (
-                datetime.datetime.now( ).strftime( '%B %d, %Y @ %I:%M:%S %p' ) )
-            logging.info( mystr )
-            self.emitString.emit( mystr )
-            self.movieDataRowsSignal.emit( self.movie_data_rows )
-
-    def _process_movie_data_rows_init( self, movie_data_rows ):
+    def process_movie_data_rows_init( self, movie_data_rows ):
         #
         ## now change everything
-        self.hide( )
+        self.progress_dialog.stopDialog( )
         self.setWindowTitle( 'PLEX MOVIE GUI' )
-        #
-        ## delete all widgets in current layout
-        while self.myLayout.count( ):
-            myWidget = self.myLayout.takeAt( 0 ).widget( )
-            myWidget.deleteLater( )
+        myLayout = QVBoxLayout( )
+        self.setLayout( myLayout )
         #
         self.tmdb_gui = plextmdb_gui.TMDBGUI(
             self.token, self.fullurl, movie_data_rows, isIsolated = False,
@@ -66,8 +63,8 @@ class TMDBTotGUI( QDialogWithPrinting ):
             self.tmdb_gui, 'The List of Movies By Genre and Year' )
         self.tabWidget.addTab(
             self.tmdb_mygui, 'My Own Movies' )
-        self.myLayout.addWidget( self.tabWidget )
-        self.myLayout.addWidget( self.statusDialog )
+        myLayout.addWidget( self.tabWidget )
+        myLayout.addWidget( self.statusDialog )
         #
         ##
         self._setupActions( )
@@ -81,24 +78,14 @@ class TMDBTotGUI( QDialogWithPrinting ):
         self.setFixedWidth( 700 )
         self.setFixedHeight( self.sizeHint( ).height( ) )
         self.show( )
-
-    def _primordial_put_strings( self, extra_string ):
-        self.primordialErrorStrings.append( extra_string )
-        myHTML = """
-        <html>
-          %s
-        </html>""" % '\n'.join(map(lambda estring: '<p>%s</p>' % estring,
-                                   self.primordialErrorStrings ) )
-        self.primordialErrorDialog.setHtml( myHTML )
         
     def __init__( self, fullurl, token, movie_data_rows = None,
                   doLarge = False, verify = True ):
         time0 = time.time( )
-        super( TMDBTotGUI, self ).__init__( None )
+        super( TMDBTotGUI, self ).__init__( None, doQuit = True )
         self.verify = verify
         self.resolution = 1.0
         if doLarge: self.resolution = 2.0
-        self.setWindowTitle( 'PLEX MOVIE GUI PROGRESS WINDOW' )
         tmdbEngine = plextmdb.TMDBEngine( verify = verify )
         self.fullurl = fullurl
         self.token = token
@@ -112,25 +99,18 @@ class TMDBTotGUI( QDialogWithPrinting ):
         self.statusDialog = QLabel( )
         self.tabWidget = QTabWidget( self )
         self.tabWidget.setStyleSheet( 'QTabBar::tab { width: 340px; }' )
-        #
-        self.setFixedWidth( 300 )
-        self.setFixedHeight( 300 )
-        self.myLayout = QVBoxLayout( )
-        self.setLayout( self.myLayout )
-        self.primordialErrorDialog = QTextEdit( )
-        self.primordialErrorDialog.setReadOnly( True )
-        self.primordialErrorStrings = [ ]
-        self.myLayout.addWidget( self.primordialErrorDialog )
-        self.show( )
+        self.hide( )
+        self.progress_dialog = ProgressDialog(
+            self, 'PLEX MOVIE GUI PROGRESS WINDOW' )
         #
         ## have this happen in background, hope it works without crashing...
-        self.mainInitThread = TMDBTotGUI.TMDBTotGUIThread(
+        self.mainInitThread = TMDBTotGUIThread(
             fullurl, token, movie_data_rows = movie_data_rows,
             verify = self.verify, time0 = time0 )
         self.mainInitThread.movieDataRowsSignal.connect(
-            self._process_movie_data_rows_init )
+            self.process_movie_data_rows_init )
         self.mainInitThread.emitString.connect(
-            self._primordial_put_strings )
+            self.progress_dialog.addText )
         self.mainInitThread.start( ) # make the magic start...
 
     def _setupActions( self ):

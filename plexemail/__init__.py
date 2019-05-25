788707d91a09c003f6a560017dd037f44b235e9f
Tanim Islam <tanim.islam@gmail.com>
2019-05-24 19:47:47 -0700

    Changes to be committed: 	modified:   plexcore/__init__.py 	modified:   plexemail/__init__.py 	modified:   plexemail/plexemail.py 	modified:   plextmdb/plextmdb_gui.py 	modified:   plextmdb/plextmdb_totgui.py

--- plexemail/__init__.py
+++ plexemail/__init__.py
@@ -1,4 +1,24 @@
 # resource file
-import os, sys
+import os, sys, base64
 mainDir = os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) )
 sys.path.append( mainDir )
+from plexcore import plexcore
+
+def send_email_lowlevel( msg ):
+    data = { 'raw' : base64.urlsafe_b64encode(
+        msg.as_bytes( ) ).decode('utf-8') }
+    #
+    credentials = plexcore.oauthGetGoogleCredentials( )
+    assert( credentials is not None )
+    email_service = build('gmail', 'v1', credentials = credentials,
+                          cache_discovery = False )
+    try: message = email_service.users( ).messages( ).send(
+            userId='me', body = data ).execute( )
+    except: print('problem with %s' % msg['To'] )
+
+    
+def send_email_localsmtp( msg ):
+    smtp_conn = smtplib.SMTP('localhost', 25 )
+    smtp_conn.ehlo( 'test' )
+    smtp_conn.sendmail( msg['From'], [ msg["To"], ], msg.as_string( ) )
+    smtp_conn.quit( )

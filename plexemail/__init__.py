451273b8a623cd1c3550794a4ec4e7c686510b53
Tanim Islam <tanim.islam@gmail.com>
2019-07-08 19:08:42 -0700

    Changes to be committed: 	modified:   ../get_mov_tor.py 	modified:   ../plexemail/__init__.py 	modified:   ../plexemail/plexmail.py 	modified:   ../plextmdb/plextmdb_torrents.py

--- plexemail/__init__.py
+++ plexemail/__init__.py
@@ -4,15 +4,6 @@ mainDir = os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) )
 sys.path.append( mainDir )
 from apiclient.discovery import build
 from plexcore import plexcore
-from . import plexemail
-
-dat = plexcore.getCredentials( verify = False )
-if dat is not None:
-    emailAddress = dat[0]
-    emailName = plexemail.get_email_contacts_dict( [ emailAddress ] )[0][0]
-else:
-    emailAddress = None
-    emailName = None
 
 def send_email_lowlevel( msg, verify = True ):
     data = { 'raw' : base64.urlsafe_b64encode(
@@ -26,10 +17,64 @@ def send_email_lowlevel( msg, verify = True ):
     try: message = email_service.users( ).messages( ).send(
             userId='me', body = data ).execute( )
     except: print('problem with %s' % msg['To'] )
-
     
 def send_email_localsmtp( msg ):
     smtp_conn = smtplib.SMTP('localhost', 25 )
     smtp_conn.ehlo( 'test' )
     smtp_conn.sendmail( msg['From'], [ msg["To"], ], msg.as_string( ) )
     smtp_conn.quit( )
+
+
+def get_email_contacts_dict( emailList, verify = True ):
+    if len( emailList ) == 0: return [ ]
+    # http = httplib2.Http( disable_ssl_certificate_validation = not verify )
+    credentials = plexcore.oauthGetGoogleCredentials( verify = verify )
+    people_service = build( 'people', 'v1', credentials = credentials,
+                            cache_discovery = False )
+    connections = people_service.people( ).connections( ).list(
+        resourceName='people/me', personFields='names,emailAddresses',
+        pageSize = 2000 ).execute( )
+    emails_dict = { }
+    for conn in filter(lambda conn: 'names' in conn and 'emailAddresses' in conn,
+                       connections['connections']):
+        name = conn['names'][0]['displayName']
+        emails = set(map(lambda eml: eml['value'], conn['emailAddresses'] ) )
+        if name not in emails_dict:
+            emails_dict[ name ] = emails
+        else:
+            new_emails = emails | emails_dict[ name ]
+            emails_dict[ name ] = new_emails
+    while 'nextPageToken' in connections: 
+        connections = people_service.people( ).connections( ).list(
+            resourceName='people/me', personFields='names,emailAddresses',
+            pageToken = connections['nextPageToken'], pageSize = 2000 ).execute( )
+        for conn in filter(lambda conn: 'names' in conn and 'emailAddresses' in conn,
+                           connections['connections']):
+            name = conn['names'][0]['displayName']
+            emails = set(map(lambda eml: eml['value'], conn['emailAddresses'] ) )
+            if name not in emails_dict:
+                emails_dict[ name ] = emails
+            else:
+                new_emails = emails | emails_dict[ name ]
+                emails_dict[ name ] = new_emails
+    #
+    emails_dict_rev = {}
+    for contact in emails_dict:
+        for email in emails_dict[contact]:
+            emails_dict_rev[ email ] = contact
+    emails_array = []
+    for email in emailList:
+        if email in emails_dict_rev:
+            emails_array.append((emails_dict_rev[ email ], email) )
+        else:
+            emails_array.append( (None, email) )
+    return emails_array
+
+
+dat = plexcore.getCredentials( verify = False )
+if dat is not None:
+    emailAddress = dat[0]
+    emailName = get_email_contacts_dict( [ emailAddress ] )[0][0]
+else:
+    emailAddress = None
+    emailName = None

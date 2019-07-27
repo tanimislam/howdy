a6ee54d206ae10e0b0fc365259ec92ec38bab864
Tanim Islam <tanim.islam@gmail.com>
2019-07-27 00:59:36 -0400

    some fix to get read the docs to work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,7 @@ _mainDir = reduce(lambda x,y: os.path.dirname( x ),
 sys.path.insert( 0, _mainDir )
 
 print( "mainDir = %s" % _mainDir)
+print( os.environ.get('READTHEDOCS'))
 #print( 'listing: %s.' % os.listdir( _mainDir ) )
 #print( 'listing #1: %s.' % os.listdir( os.path.dirname( _mainDir ) ) )
 #print( 'listing #2: %s.' % os.listdir( os.path.dirname( os.path.dirname( _mainDir ) ) ) )

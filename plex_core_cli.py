4726d1dab110c72a4151aef7e19b8292ef900f66
Tanim Islam <islam5@llnl.gov>
2019-07-26 23:06:25 -0400

    Changes to be committed: 	modified:   ../../.readthedocs.yml 	modified:   ../../plex_core_cli.py

--- plex_core_cli.py
+++ plex_core_cli.py
@@ -20,22 +20,16 @@ def main( ):
     parser.add_option( '--guestemail', dest='guest_email', action = 'store', type=str,
                        help = 'Name of the Plex guest email.' )
     parser.add_option( '--newemails', dest='new_emails', action = 'store', type = str,
-                       help = 'Name of the new emails associated with the Plex guest email.')
+                      help = 'Name of the new emails associated with the Plex guest email.')
     parser.add_option( '--replace_existing', dest='do_replace_existing', action = 'store_true', default = False,
                        help = 'If chosen, replace existing email to send newsletter to.')
     parser.add_option( '--mappedfriends', dest='do_mapped_friends', action='store_true', default = False,
                        help = 'Get list of guests with mapping, of your Plex server.')
-    parser.add_option( '--updateflaskcreds', dest='do_updateflaskcreds', action = 'store_true', default = False,
-                       help = 'Update flask credentials.' )
-    parser.add_option( '--flaskpassword', dest='flaskpassword', action = 'store', type=str,
-                       help = 'Password to flask server for Tanim Islam.' )
-    parser.add_option( '--justadd', dest='do_justadd', action = 'store_true', default = False,
-                       help = 'Just add the set of emails we set with --newemails.' )
     opts, args = parser.parse_args( )
     assert(len(list(
-        filter(lambda tok: tok is True, ( opts.do_friends, opts.do_addmapping,
-                                          opts.do_mapped_friends, opts.do_updateflaskcreds,
-                                          opts.do_justadd ) ) ) ) == 1 )
+        filter(lambda tok: tok is True, (
+            opts.do_friends, opts.do_addmapping,
+            opts.do_mapped_friends ) ) ) ) == 1 )
     if any(map(lambda tok: tok is None, ( opts.username, opts.password ) ) ):
         var = plexcore.checkServerCredentials( doLocal = False, verify = False )
         if var is None:
@@ -120,43 +114,6 @@ def main( ):
                            '---' ]) )
         for email in mapped_emails:
             print( email )
-    elif opts.do_updateflaskcreds:
-        assert( opts.flaskpassword is not None ), "must have a password to access these flask server credentials."
-        response = requests.get( 'https://tanimislam.ddns.net/flask/accounts/checkpassword',
-                                 auth = ( 'tanim.islam@gmail.com', opts.flaskpassword ) )
-        if response.status_code != 200:
-            print( 'ERROR, WRONG PASSWORD FOR TANIM ISLAM.' )
-            return
-        query = session.query( plexcore.PlexGuestEmailMapping )
-        subtracts = [ ]
-        adds = [ ]
-        for mapping in query.all( ):
-            replace_existing = mapping.plexreplaceexisting
-            plex_email = mapping.plexemail
-            if replace_existing: subtracts.append( plex_email )
-            adds += map(lambda tok: tok.strip(), mapping.plexmapping.strip().split(','))
-        adds = sorted( set( adds ) )
-        response = requests.post( 'https://tanimislam.ddns.net/flask/accounts/adddeleteusersbulk',
-                                  json = { 'initialpassword' : 'initialplexserver',
-                                           'addemails' : adds, 'deleteemails' : subtracts },
-                                  auth = ( 'tanim.islam@gmail.com', opts.flaskpassword ) )
-        data = response.json( )
-        print( '\n'.join(map(lambda tok: data[tok], ( 'deleted', 'added' ) ) ) )
-    elif opts.do_justadd:
-        assert( opts.flaskpassword is not None )
-        assert( opts.new_emails is not None )
-        response = requests.get( 'https://tanimislam.ddns.net/flask/accounts/checkpassword',
-                                 auth = ( 'tanim.islam@gmail.com', opts.flaskpassword ) )
-        if response.status_code != 200:
-            print( 'ERROR, WRONG PASSWORD FOR TANIM ISLAM.' )
-            return
-        adds = sorted( set( map(lambda email: email.strip(), opts.new_emails.split(',') ) ) )
-        response = requests.post( 'https://tanimislam.ddns.net/flask/accounts/adddeleteusersbulk',
-                                  json = { 'initialpassword' : 'initialplexserver',
-                                           'addemails' : adds },
-                                  auth = ( 'tanim.islam@gmail.com', opts.flaskpassword ) )
-        data = response.json( )
-        print( data['added'] )
         
 if __name__=='__main__':
     main( )

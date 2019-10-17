fe42df6dffe9398f0157ff3f09b487136c2d22bb
Tanim Islam <tanim.islam@gmail.com>
2019-10-16 22:02:06 -0700

    Substantial changes in documentation, fixes in some command line executable functionality, and improvements in underlying API
    1. docs/source/create_exec_covg.py: printout of ReST table of Plexstuff CLI and GUIs also prints out a checkbox if documentation on the executable is finished.
    
    2. docs/source/index.rst: ReST table of Plexstuff CLI and GUI now shows checkbox if documentation on executable is finished.
    
    3. plex_resynclibs.py: fundamental changes in the functionality of plex_resynclibs.py. Documentation in progress...
    
    4. plexcore/__init__.py: improved get_formatted_size( ) to allow for terabytes, get_formatted_duration( ) to use singular for things like "1 day, 1 hour, etc."
    
    5. plexcore/plexcore.py: many changes
    
    	a. plex_owned_servers( ) removed. plex_all_servers( ) now returns a dictionary of all servers to which one has access. Fixes to checkServerCredentials( ) to reflect this.
    
    	b. _get_library_stats_movie( ), _get_library_stats_show( ), and _get_library_stats_artist( ) fixed due to changes in associated _get_library_data...( ) methods. get_library_stats( ) now returns a dictionary. Documentation of get_library_stats( ) is complete.
    
    	c. improved get_lastN_movies( ) to allow for multiple different Plex movie libraries.
    
    6. plexemail/plexemail.py: some changes
    
    	a. changed get_summary_data_music_remote( ), get_summary_data_television_remote( ), and get_summary_data_movie_remote( ) given changes in get_library_states( ) in plexcore/plexcore.py. Also changed function to now look through ALL Plex libraries that are movies, TV shows, or music, rather than a single library.
    
    	b. removed get_summary_data_movies( ) because very limited to a specific Plex server configuration.
    
    7. plextmdb/plextmdb.py: fixes to getMovieData( )
    
    Changes to be committed:
    	modified:   docs/source/create_exec_covg.py
    	modified:   docs/source/index.rst
    	modified:   plex_resynclibs.py
    	modified:   plexcore/__init__.py
    	modified:   plexcore/plexcore.py
    	modified:   plexemail/plexemail.py
    	modified:   plextmdb/plextmdb.py

--- plex_resynclibs.py
+++ plex_resynclibs.py
@@ -1,40 +1,51 @@
 #!/usr/bin/env python3
 
+import sys, textwrap
+from os import get_terminal_size
+from tabulate import tabulate
 from optparse import OptionParser
 from plexcore import plexcore, get_formatted_duration, get_formatted_size
 
-def _print_summary( library_key, library_dict, token = None, fullURL = 'http://localhost:32400' ):
-    data = plexcore.get_library_stats( library_key, token = token, fullURL = fullURL )
-    fullURL, title, mediatype = data[:3]
+
+def _print_summary( library_key, library_dict, token, fullURL ):
+    data = plexcore.get_library_stats( library_key, token, fullURL = fullURL )
+    mediatype = data[ 'mediatype' ]
+    title = data[ 'title' ]
+    columns = min( 100, get_terminal_size( ).columns )
     if mediatype == 'movie':
-        num_movies, totdur, totsize = data[3:]
-        print( ' '.join([ '%s is a movie library.' % library_dict[ library_key ],
-                          'There are %d movies here.' % num_movies,
-                          'The total size of movie media is %s.' %
-                          get_formatted_size( totsize ),
-                          'The total duration of movie media is %s.' % 
-                          get_formatted_duration( totdur ) ]) )
+        mystr = ' '.join([ '"%s" is a movie library.' % title,
+                           'There are %d movies here.' % data[ 'num_movies' ],
+                           'The total size of movie media is %s.' %
+                           get_formatted_size( data[ 'totsize' ] ),
+                           'The total duration of movie media is %s.' % 
+                           get_formatted_duration( data[ 'totdur' ] ) ] )
     elif mediatype == 'show':
-        num_tveps, num_tvshows, totdur, totsize = data[3:]
-        print( ' '.join([ '%s is a TV library.' % library_dict[ library_key ],
-                          'There are %d TV files in %d TV shows.' % ( num_tveps, num_tvshows ),
-                          'The total size of TV media is %s.' %
-                          get_formatted_size( totsize ),
-                          'The total duration of TV shows is %s.' % 
-                          get_formatted_duration( totdur ) ]) )
+        mystr =' '.join([ '"%s" is a TV library.' % title,
+                              'There are %d TV files in %d TV shows.' % (
+                                  data[ 'num_tveps' ], data[ 'num_tvshows' ] ),
+                              'The total size of TV media is %s.' %
+                              get_formatted_size( data[ 'totsize' ] ),
+                              'The total duration of TV shows is %s.' % 
+                              get_formatted_duration( data[ 'totdur' ] ) ] )
     elif mediatype == 'artist':
         num_songs, num_albums, num_artists, totdur, totsize = data[3:]
-        print( ' '.join([ '%s is a music library.' % library_dict[ library_key ],
-                          'There are %d songs made by %d artists in %d albums.' %
-                          ( num_songs, num_artists, num_albums ),
-                          'The total size of music media is %s.' %
-                          get_formatted_size( totsize ),
-                          'The total duration of music media is %s.' %
-                          get_formatted_duration( totdur ) ]) )
+        num_songs = data[ 'num_songs' ]
+        num_artists = data[ 'num_artists' ]
+        num_albums = data[ 'num_albums' ]
+        totsize = data[ 'totsize' ]
+        totdur = data[ 'totdur' ]
+        mystr = ' '.join([ '"%s" is a music library.' % title,
+                           'There are %d songs made by %d artists in %d albums.' %
+                           ( num_songs, num_artists, num_albums ),
+                           'The total size of music media is %s.' %
+                           get_formatted_size( totsize ),
+                           'The total duration of music media is %s.' %
+                           get_formatted_duration( totdur ) ] )
+    print( '\n%s\n' % '\n'.join( textwrap.fill( mystr, width = columns ).split('\n') ) )    
 
 def main( ):
     parser = OptionParser( )
-    parser.add_option('--librarynames', dest='do_librarynames', action='store_true', default=False,
+    parser.add_option('--libraries', dest='do_libraries', action='store_true', default=False,
                       help = 'If chosen, just give the sorted names of all libraries in the Plex server.')
     parser.add_option('--refresh', dest='do_refresh', action='store_true', default=False,
                       help = 'If chosen, refresh a chosen library in the Plex server. Must give a valid name for the library.')
@@ -42,92 +53,91 @@ def main( ):
                       help = 'If chosen, perform a summary of the chosen library in the Plex server. Must give a valid name for the library.')
     parser.add_option('--library', dest='library', type=str, action='store',
                       help = 'Name of a (valid) library in the Plex server.')
-    parser.add_option('--remote', dest='do_remote', action='store_true', default = False,
-                      help = 'If chosen, use the given Plex username and password to get the libraries.' )
-    parser.add_option('--username', dest='username', action='store', type=str,
-                      help = 'Plex user email.' )
-    parser.add_option('--password', dest='password', action='store', type=str,
-                      help = 'Plex password.' )
-    parser.add_option('--servername', dest='servername', action='store', type=str,
+    parser.add_option('--servername', dest='servername', action='store', type = str,
                       help = 'Optional name of the server to check for.' )
     parser.add_option('--servernames', dest='do_servernames', action='store_true', default=False,
                       help = 'If chosen, print out all the servers owned by the user.')
+    parser.add_option('--noverify', dest='do_verify', action='store_false', default = True,
+                      help = 'Do not verify SSL transactions if chosen.' )
     opts, args = parser.parse_args( )
-    if not opts.do_remote:
-        assert( len( list( filter( lambda tok: tok is True, ( opts.do_librarynames, opts.do_refresh, opts.do_summary ) ) ) ) == 1 )
-        #
-        ## first get the token
-        data = plexcore.checkServerCredentials( doLocal = True )
-        if data is None:
-            print('Sorry, now we need to provide an user name and password. Please get one!')
-            return
-        _, token = data
-        library_dict = plexcore.get_libraries( token )
-        if opts.do_librarynames:
-            print('Here are the %d libraries in this Plex server.' % len( library_dict ) )
-            for libraryName in sorted( library_dict.values( ) ):
-                print( libraryName )
-            return
-        assert( opts.library is not None )
-        if opts.library not in library_dict.values( ):
-            print('ERROR, candidate library %s not in %s.' % ( opts.library, sorted( set( library_dict.values( ) ) ) ) )
-            return      
-        library_key = max(filter(lambda key: library_dict[ key ] == opts.library, library_dict ) )
-        if opts.do_refresh:
-            plexcore.refresh_library( library_key, library_dict, token = token )
-            return
-        _print_summary( library_key, library_dict, token = token )
-    else:
-        assert( len( list( filter( lambda tok: tok is True, ( opts.do_librarynames, opts.do_refresh, opts.do_servernames,
-                                                        opts.do_summary ) ) ) ) == 1 )
-        assert( opts.username is not None )
-        assert( opts.password is not None )
-        token = plexcore.getTokenForUsernamePassword( opts.username, opts.password )
-        if token is None:
-            print( 'INVALID USERNAME/PASSWORD' )
-            return
-        server_dicts = plexcore.get_all_servers( token )
+    #
+    ##
+    _, token = plexcore.checkServerCredentials( doLocal = False, verify = opts.do_verify )
+    #
+    ## only one of possible actions
+    assert( len( list( filter( lambda tok: tok is True, (
+        opts.do_libraries, opts.do_refresh, opts.do_summary, opts.do_servernames ) ) ) ) == 1 ), \
+        "error, must choose one of --libraries, --refresh, --summary, --servernames"
+    
+    #
+    ## if list of servernames, --servernames
+    if opts.do_servernames:
+        server_dicts = plexcore.get_all_servers( token, verify = opts.do_verify )
         if server_dicts is None:
-            print( 'COULD FIND NO SERVERS OWNED OR ACCESIBLE TO %s.' % opts.username )
+            print( 'COULD FIND NO SERVERS ACCESIBLE TO USER.' )
             return
-        if opts.do_servernames:
-            server_names = sorted( server_dicts[ 'owned' ].keys( ) )
-            print( 'SERVERS OWNED BY %s:' % opts.username )
-            for server_name in server_names:
-                print( '%s => %s' % ( server_name, server_dicts[ 'owned' ][ server_name ] ) )
-            server_names = sorted( server_dicts[ 'unowned' ].keys( ) )
-            print( 'SERVERS ACCESSIBLE TO %s:' % opts.username )
-            for server_name in server_names:
-                print( '%s => %s' % ( server_name, server_dicts[ 'unowned' ][ server_name ] ) )
-            return
-        if opts.servername is None:
-            servername = min( server_dicts[ 'owned' ] )
-        else:
-            servername = opts.servername
-        server_dicts_all = { }
-        for stat in [ 'owned', ]:
-            for name in server_dicts[ stat ]:
-                server_dicts_all[ name ] = ( server_dicts[ stat ][ name ], stat )
-        assert( servername in server_dicts_all.keys( ) )
-        fullURL = 'https://%s' % server_dicts_all[ servername ][ 0 ]
-        library_dict = plexcore.get_libraries(
-            token, fullURL = fullURL )
-        if opts.do_librarynames:
-            print('Here are the %d libraries on Plex server %s (%s).' %
-                  ( len( library_dict ), servername, fullURL ) )
-            for libraryName in sorted( library_dict.values( ) ):
-                print( libraryName )
-            return
-        assert( opts.library is not None )
-        if opts.library not in library_dict.values( ):
-            print('ERROR, candidate library %s not in %s.' % (
-                opts.library, sorted( set( library_dict.values( ) ) ) ) )
-            return
-        library_key = max(filter(lambda key: library_dict[ key ] == opts.library, library_dict ) )
-        if opts.do_refresh:
-            plexcore.refresh_library( library_key, library_dict, fullURL = fullURL, token = token )
-            return
-        _print_summary( library_key, library_dict, token = token, fullURL = fullURL )
+        server_formatted_data = list(
+            map(lambda name:
+                ( name, server_dicts[ name ][ 'owned' ],
+                  server_dicts[ name ][ 'url' ] ), server_dicts ) )
+        print( '\n%s\n' %
+               tabulate( server_formatted_data, headers = [ 'Name', 'Is Owned', 'URL' ] ) )
+        return
+
+    #
+    ## check that server name we choose is owned by us.
+    server_dicts = plexcore.get_all_servers( token, verify = opts.do_verify )
+    server_names_owned = sorted(
+        set(filter(lambda name: server_dicts[ name ][ 'owned' ],
+                   server_dicts ) ) )
+    assert( len( server_names_owned ) > 0 ), "error, none of these Plex servers is owned by us."
+    if opts.servername is None:
+        opts.servername = max( server_names_owned )
+    
+    assert( opts.servername in server_names_owned ), "error, server %s not in list of owned servers: %s." % (
+        opts.servername, server_names_owned )
+
+    #
+    ## get URL and token from server_dicts
+    fullURL = server_dicts[ opts.servername ][ 'url' ]
+    token = server_dicts[ opts.servername ][ 'access token' ]
+
+    #
+    ## if get list of libraries, --libraries
+    if opts.do_libraries:
+        library_dict = plexcore.get_libraries( token, fullURL = fullURL, do_full = True )
+        print( '\nHere are the %d libraries in this Plex server: %s.' % (
+            len( library_dict ), opts.servername ) )
+        libraries_library_dict = dict(map(lambda keynum: ( library_dict[ keynum ][ 0 ], library_dict[ keynum ][ 1 ] ),
+                                          library_dict.keys( ) ) )        
+        library_names = sorted( libraries_library_dict )
+        libraries_formatted_data = list(
+            map(lambda name: ( name, libraries_library_dict[ name ] ), library_names ) )
+        print( '\n%s\n' %
+               tabulate( libraries_formatted_data, headers = [ 'Name', 'Library Type' ] ) )
+        return
+
+    #
+    ## now gone through here, must define a --library
+    assert( opts.library is not None ), "error, library must be defined."
+    library_dict = plexcore.get_libraries( token, fullURL = fullURL, do_full = True )
+    library_names = sorted(map(lambda keynum: library_dict[ keynum ][ 0 ], library_dict.keys( ) ) )
+    assert( opts.library in library_names ), "error, library = %s not in %s." % (
+        opts.library, library_names )
+    library_key = max(filter(lambda keynum: library_dict[ keynum ][ 0 ] == opts.library, library_dict ) )
+
+    #
+    ## if summary is chosen, --summary
+    if opts.do_summary:
+        _print_summary( library_key, library_dict, token, fullURL )
+        return
+
+    #
+    ## otherwise refresh is chosen, --refresh
+    if opts.do_refresh:
+        plexcore.refresh_library( library_key, library_dict, fullURL = fullURL, token = token )
+        print( 'refreshed library %s.' % opts.library )
+        return
         
 if __name__=='__main__':
     main( )

ab723191853f1ad1c17f310574547f54a5155a20
Tanim Islam <tanim.islam@gmail.com>
2019-10-22 00:17:02 -0700

    I have done the following main things:
    1. fleshed out docstrings documentation for plextvdb/__init__.py and MOST of plextvdb/plextvdb.py (not the documentation on plextvdb.plextvdb.TVShow and plextvdb.plextvdb.TVSeason objects). Wrote out more documentation in plex-tvdb/plex_tvdb_api.rst.
    
    2. plex_tvdb_epname.py: the episode name now follows titlecase format. Improved the logic of choosing SSL verification.
    
    3. Moved plextvdb/plextvdb_other.py to plextvdb/plextvdb_attic.py, to reflect functionality that is old, not tested or exercised, and possibly broken and not understood. Moved some unused and misunderstood methods from plextvdb/plextvdb.py into plextvdb/plextvdb_attic.py.
    
    4. Used plexcore.return_error_raw(..) to return error messages in plextvdb/plextvdb_torrents.py.
    
    Changes to be committed:
    	new file:   docs/source/_static/tvdb_simpsons_info.json
    	modified:   docs/source/plex-tvdb/plex_tvdb_api.rst
    	modified:   plex_tvdb_epname.py
    	modified:   plexcore/__init__.py
    	modified:   plextvdb/__init__.py
    	modified:   plextvdb/plextvdb.py
    	renamed:    plextvdb/plextvdb_other.py -> plextvdb/plextvdb_attic.py
    	modified:   plextvdb/plextvdb_torrents.py

--- plextvdb/__init__.py
+++ plextvdb/__init__.py
@@ -5,10 +5,23 @@ from sqlalchemy import Column, String
 _mainDir = reduce(lambda x,y: os.path.dirname( x ), range( 2 ),
                   os.path.abspath( __file__ ) )
 sys.path.append( _mainDir )
+
 from plexcore import session, create_all, PlexConfig, Base
 from sqlalchemy import String, Column
 
 class ShowsToExclude( Base ): # these are shows you want to exclude
+    """
+    This SQLAlchemy_ ORM class contains the list of shows to exclude from analysis. These shows must exist on the Plex_ server. Stored into the ``showstoexclude`` table in the SQLite3_ configuration database.
+
+    Attributes:
+        show: the show, that exists on the Plex_ server, to exclude from analysis and update.
+
+    .. _TVDB: https://api.thetvdb.com/swagger
+    .. _SQLAlchemy: https://www.sqlalchemy.org
+    .. _Plex: https://plex.tv
+    .. _SQLite3: https://www.sqlite.org/index.html
+    """
+    
     #
     ## create the table using Base.metadata.create_all( _engine )
     __tablename__ = 'showstoexclude'
@@ -20,6 +33,17 @@ class ShowsToExclude( Base ): # these are shows you want to exclude
 create_all( )
     
 def save_tvdb_api( username: str, apikey: str, userkey: str, verify = True ):
+    """
+    Saves the information on the TVDB_ API access into the ``tvdb`` configuration service in the ``plexconfig`` table. Details of how to set up the configuration in :ref:`TVDB API configuation <The Television Database (TVDB) API>`, which uses ``plex_config_gui.py``.
+
+    :param str username: the TVDB_ API username.
+    :param str apikey: the TVDB_ API key.
+    :param str userkey: the TVDB_ API user key.
+    :param bool verify: optional argument, whether to verify SSL connections. Default is ``True``.
+
+    :returns: a satus :py:class:`string <str>`. If successful, returns ``"SUCCESS"``. If unsuccessful, returns ``"FAILURE, COULD NOT SAVE TVDB API STUFF"``.
+    :rtype: str
+    """
     #
     ## first check if works
     isValid = check_tvdb_api( username, apikey, userkey, verify = verify )
@@ -47,6 +71,22 @@ def check_tvdb_api( username, apikey, userkey, verify = True ) -> bool:
     return True
 
 def get_tvdb_api( ) -> dict:
+    """
+    Returns the :py:class:`dictionary <dict>` of TVDB_ API credentials (see  :ref:`TVDB API configuation <The Television Database (TVDB) API>`), taken from the ``tvdb`` configuration service in the ``plexconfig`` table. The form of the dictionary is,
+
+    .. code-block:: python
+
+       {
+         'username' : USERNAME, # the TVDB API user name
+         'apikey'   : APIKEY,   # the TVDB API key
+         'userkey'  : USERKEY   # THE TVDB API user key
+       }
+
+    :returns: the :py:class:`dictionary <dict>` of TVDB_ API credentials.
+    :rtype: dict
+    :raises: ValuerError: if the TVDB_ API credentials are not found in the ``plexconfig`` table.
+    
+    """
     query = session.query( PlexConfig ).filter( PlexConfig.service == 'tvdb' )
     val = query.first( )
     if val is None:
@@ -57,7 +97,18 @@ def get_tvdb_api( ) -> dict:
              'userkey' : data['userkey'] }
 
 def get_token( verify: bool = True, data = None ) -> str:
-    if data is None: data = get_tvdb_api( )
+    """
+    Returns the TVDB_ API token that allows access to the TVDB_ database. If there are errors, then returns :py:class:`None`.
+
+    :param bool verify: optional argument, whether to verify SSL connections. Default is ``True``.
+    :param dict data: optional argument. If provided, must be a dictionary containing the TVDB_ API credentials as described in :py:meth:`get_tvdb_api( ) <plextvdb.plextvdb.get_tvdb_api>`. Otherwise, returns the dictionary returned by :py:meth:`get_tvdb_api( ) <plextvdb.plextvdb.get_tvdb_api>`.
+
+    :returns: the TVDB_ API :py:class:`string <str>` token, otherwise returns :py:class:`None` if there are errors.
+    :rtype: str
+    """
+    if data is None:
+        try: data = get_tvdb_api( )
+        except: return None
     headers = { 'Content-Type' : 'application/json' }
     response = requests.post( 'https://api.thetvdb.com/login',
                               data = json.dumps( data ),
@@ -66,6 +117,14 @@ def get_token( verify: bool = True, data = None ) -> str:
     return response.json( )[ 'token' ]
 
 def refresh_token( token: str, verify: bool = True ):
+    """
+    Refreshes the TVDB_ API token.
+
+    :param str token: a previous valid TVDB_ API access token.
+    :param bool verify:  optional argument, whether to verify SSL connections. Default is ``True``.
+    :returns: a new TVDB_ API :py:class:`string <str>` token. If there are others, returns :py:class:`None`.
+    :rtype: str
+    """
     headers = { 'Content-Type' : 'application/json',
                 'Authorization' : 'Bearer %s' % token }
     response = requests.get( 'https://api.thetvdb.com/refresh_token',

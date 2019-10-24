7e404ab4d9846f88e9b56db43e13bb7105f0a0da
Tanim Islam <tanim.islam@gmail.com>
2019-10-24 13:36:31 -0700

    1. plexmusic/plexmusic.py:    a. some improvements to PlexLastFM object: now can download album image when not verifying SSL connections.
       b. some improvements to MusicInfo object: Fixing print formatting for all albums for an artist, using plexcore.return_error_raw( ) method for error messages with output.
    
    2. plex_music_album.py: now using plexmusic.plexmusic.PlexLastFM object instead of nonworking plexmusic.plexmusic.PlexMusic object for handling song information; all functionalities now work and print formatting much improved.
    
    Changes to be committed:
    	modified:   plex_music_album.py
    	modified:   plexmusic/plexmusic.py

--- plex_music_album.py
+++ plex_music_album.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-import codecs, os, sys, signal
+import codecs, os, sys, signal, tabulate
 def signal_handler( signal, frame ):
     print( "You pressed Ctrl+C. Exiting...")
     sys.exit( 0 )
 signal.signal( signal.SIGINT, signal_handler )
 from plexmusic import plexmusic
+from plexcore import plexcore
 from optparse import OptionParser
 
-if __name__=='__main__':
+def main( ):
     parser = OptionParser( )
     parser.add_option( '-a', '--artist', dest='artist_name', type=str, action='store',
                       help = 'Name of the artist to get album image for.' )
@@ -18,36 +19,44 @@ if __name__=='__main__':
                       help = 'If chosen, get the song listing instead of downloading the album image.')
     parser.add_option( '--formatted', dest='do_formatted', action='store_true', default = False,
                       help = ' '.join([
-                                       'If chosen, print the song listing in a format recognized by plex_music_metafill.py'
-                                       'for downloading a collection of songs.' ]) )
-    parser.add_option('--all', dest='do_all', action='store_true', default = False,
+                          'If chosen, print the song listing in a format recognized by plex_music_metafill.py',
+                          'for downloading a collection of songs.' ]) )
+    parser.add_option('--albums', dest='do_albums', action='store_true', default = False,
                       help = 'If chosen, then get a list of all the songs in all studio albums for the artist.' )
     parser.add_option( '--debug', dest='do_debug', action='store_true', default=False,
                        help = 'Run with debug mode turned on.' )
     parser.add_option('--noverify', dest='do_verify', action='store_false', default = True,
                       help = 'If chosen, do not verify SSL connections.' )
     opts, args = parser.parse_args( )
-    assert(all(map(lambda tok: tok is not None, ( opts.artist_name,  ) ) ) )
-    if not opts.do_all: assert( opts.album_name is not None )
+    assert( opts.artist_name is not None )
+    if not opts.do_albums: assert( opts.album_name is not None )
     #
     ##    
-    pm = plexmusic.PlexMusic( verify = opts.do_verify )
-    if opts.do_all:
+    plastfm = plexmusic.PlexLastFM( verify = opts.do_verify )
+    if opts.do_albums:
         username, _ = plexcore.getCredentials( verify = False, checkWorkingServer = False )
         plexmusic.MusicInfo.get_set_musicbrainz_useragent( username )
-        mi = plexmusic.MusicInfo( opts.artist_name )
-        albums_sorted = sorted(mi.alltrackdata.keys( ), key = lambda album:
-                               album[ 'release-date' ] )[::-1]
-        print( 'found %d albums for %s.' % ( len( albums_sorted ), opts.artist_name ) )
-        for album in albums_sorted:
-            print( '%s, %d.' % ( album, mi.alltrackdata[ album ][ 'release-date' ].year ) )
-    if not opts.do_songs:
-        _, status = pm.get_album_image( opts.artist_name, opts.album_name )
-        if status != 'SUCCESS': print( status )
+        mi = plexmusic.MusicInfo( opts.artist_name.strip( ) )
+        mi.print_format_album_names( )
+        return
+    
+    if not opts.do_songs: # just get the song image
+        _, status = plastfm.get_album_image( opts.artist_name, opts.album_name )
+        if status != 'SUCCESS':
+            print( status )
+        return
+
+    track_listing, status = plastfm.get_song_listing(
+        opts.artist_name, album_name = opts.album_name )
+    if status != 'SUCCESS':
+        print( status )
+        return
+
+    if not opts.do_formatted:
+        print( '\n%s\n' %
+               tabulate.tabulate( track_listing, headers = [ 'Song', 'Track #' ] ) )
     else:
-        track_listing = pm.get_song_listing( opts.artist_name, album_name = opts.album_name )
-        if not opts.do_formatted:
-            for title, trkno in track_listing:
-                print( '%02d - %s' % ( trkno, title ) )
-        else:
-            print( ';'.join(map(lambda title_trkno: title_trkno[0], track_listing)) )
+        print( '\n%s\n' % ';'.join(map(lambda title_trkno: title_trkno[0], track_listing)) )
+
+if __name__ == '__main__':
+    main( )

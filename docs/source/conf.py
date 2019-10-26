567e68bfc788cd74252f8ecf3e6bbf0f89c26b7d
Tanim Islam <tanim.islam@gmail.com>
2019-10-26 06:02:54 -0700

    1. Changed default to dark mode for Sphinx documentation.
    2. Added a "TODO" section for plexstuff music functionality.
    
    Changes to be committed:
    	new file:   docs/source/_static/rtd_dark.css
    	modified:   docs/source/conf.py
    	modified:   docs/source/plex-music/plex_music.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -154,6 +154,8 @@ html_sidebars = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = [ 'rtd_dark.css' ]
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #

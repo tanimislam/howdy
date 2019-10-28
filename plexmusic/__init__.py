b4f593bcc5df605c5c6a971edaad0aa7e5ab57c0
Tanim Islam <islam5@llnl.gov>
2019-10-28 00:30:24 -0700

    Made improvements and updates to the plexmusic API, and documented plexmusic and plexmusic.plexmusic modules. Also, now beginning the removal of email/password authentication for the GMusicAPI Mobileclient music manager -- using oauth2 authentication.
    1. source/plex-music/plex_music_api.rst, plexmusic/__init__.py, and plexmusic/plexmusic.py: finished plexmusic and plexmusic.plexmusic API documentation. Will leave plexmusic.pygn undocumented, since copied from https://github.com/cweichen/pygn.
    
    2. plexcore/plexcore_gui.py: started to look forward to a near future in which email/password authentication not used. Started by defining a GUI front-end to plexmusic.oauth_store_google_credentials and plexmusic.oauth_get_google_credentials with GMusicMobileClientOauth2Dialog GUI widget.
    
    3. plexmusic/plexmusic.py: improved plexmusic.get_gmusicmanager method to now work more robustly, and in situations where SSL connections are not verified. Changed Mobileclient authentication to use OAuth2 instead of user/password. Also, moved download_best_song routine into plexmusic.py, deleting plexmusic_ydl.py.
    
    4. plextvdb/plextvdb_attic.py: changed todo directive to warning directive.
    
    Changes to be committed:
    	new file:   docs/source/_static/lastfm_collection_data.json
    	modified:   docs/source/plex-music/plex_music_api.rst
    	modified:   plexcore/plexcore_gui.py
    	modified:   plexmusic/__init__.py
    	modified:   plexmusic/plexmusic.py
    	deleted:    plexmusic/plexmusic_ydl.py
    	modified:   plextvdb/plextvdb_attic.py

--- plexmusic/__init__.py
+++ plexmusic/__init__.py
@@ -8,11 +8,28 @@ sys.path.append( _mainDir )
 _dt0 = datetime.datetime.strptime("00", "%S")
 
 def parse_youtube_date( duration_string ):
+    """
+    Parses a duration string that ``isodate`` can recognize, such as ``PT4M33S``, into a :py:class:`datetime <datetime.datetime>`.
+    
+    :param str duration_string: duration in ``isodate`` format, such as ``PT4M33S`` to represent 4 minutes and 33 seconds.
+    :returns: the :py:class:`datetime <datetime.datetime>` that can be used to infer duration using :py:meth:`format_youtube_date <plexmusic.format_youtube_date>`.
+    :rtype: :py:class:`datetime <datetime.datetime>`
+
+    .. seealso:: :py:meth:`format_youtube_date <plexmusic.format_youtube_date>`
+    """
     dstring = duration_string.upper( )
     timedelta = isodate.parse_duration( dstring )
     return _dt0 + timedelta
 
 def format_youtube_date( dt_duration ):
+    """    
+    :param datetime dt_duration: the :py:class:`datetime <datetime.datetime>`
+    
+    :returns: a standard second, or MM:SS (such as 4:33), or so on from a :py:class:`datetime <datetime.datetime>` returned by :py:meth:`parse_youtube_date <plexmusic.parse_youtube_date>`.
+    :rtype: str
+
+    .. seealso:: :py:meth:`parse_youtube_date <plexmusic.parse_youtube_date>`
+    """
     delta = relativedelta( dt_duration, _dt0 )
     if delta.days >= 20:
         raise ValueError("Error, duration is longer than 20 days!")
@@ -28,6 +45,13 @@ def format_youtube_date( dt_duration ):
 
 
 def get_failing_artistalbum( filename ):
+    """
+    :param str filename: a candidate M4A_ music file name (file must also end in ``".m4a"``).
+    :returns: the filename ONLY IF it is missing either album or artist metadata. Otherwise returns ``None``.
+    :rtype: str
+
+    .. _M4A: https://en.wikipedia.org/wiki/MPEG-4_Part_14
+    """
     if not os.path.basename( filename ).endswith( '.m4a' ):
         return None
 

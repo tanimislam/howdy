2674a6a1fe75ab0f3941ccca73ab652f1f06ecb7
Tanim Islam <tanim.islam@gmail.com>
2019-10-28 23:40:39 -0700

    1. source/plex-core/cli_tools/plex_core_cli.rst and plex_store_credentials.py: allow for non-verified SSL connections, and change documentation to reflect that.
    2. source/plex-music/cli_tools/plex_music_cli.rst: finished all documentation except for plex_music_songs.py. Plan to change its functionality slightly so it does not overlap with plex_music_album.py, and write up documentation.
    
    3. plexmusic/plexmusic.py: minor fix to documentation.
    
    4. upload_to_gmusic.py: fixed functionality that allows one to store the GMusicAPI Mobileclient OAuth2 authentication into the configuration database.
    
    Changes to be committed:
    	modified:   docs/source/plex-core/cli_tools/plex_core_cli.rst
    	new file:   docs/source/plex-music/cli_tools/plex-music-cli-figures/plex_music_metafill_album.gif
    	new file:   docs/source/plex-music/cli_tools/plex-music-cli-figures/plex_music_metafill_album.mp4
    	new file:   docs/source/plex-music/cli_tools/plex-music-cli-figures/plex_music_metafill_songs.gif
    	new file:   docs/source/plex-music/cli_tools/plex-music-cli-figures/plex_music_metafill_songs.mp4
    	modified:   docs/source/plex-music/cli_tools/plex_music_cli.rst
    	modified:   plex_store_credentials.py
    	modified:   plexmusic/plexmusic.py
    	modified:   upload_to_gmusic.py

--- upload_to_gmusic.py
+++ upload_to_gmusic.py
@@ -6,7 +6,8 @@ def signal_handler( signal, frame ):
     print( "You pressed Ctrl+C. Exiting...")
     sys.exit( 0 )
 signal.signal( signal.SIGINT, signal_handler )
-import os, glob
+import os, glob, gmusicapi, httplib2
+from oauth2client.client import OAuth2WebServerFlow
 from optparse import OptionParser
 
 from plexmusic import plexmusic
@@ -18,12 +19,25 @@ def _files_from_commas(fnames_string):
 def _files_from_glob(fnames_string):
     return set(filter(lambda fname: os.path.isfile(fname), glob.glob(fnames_string)))
 
-if __name__=='__main__':
+def _get_oauth_credentials( ):
+    oauth_creds = dict( gmusicapi.session.Mobileclient.oauth._asdict( ) )
+    flow = OAuth2WebServerFlow(
+        client_id = oauth_creds[ 'client_id' ],
+        client_secret = oauth_creds[ 'client_secret' ],
+        scope = oauth_creds[ 'scope' ],
+        redirect_uri = oauth_creds[ 'redirect_uri' ] )
+    auth_uri = flow.step1_get_authorize_url( )
+    return flow, auth_uri
+    
+
+def main( ):
     parser = OptionParser()
     parser.add_option('-f', '--filenames', dest='filenames', action='store', type=str,
                       help = 'Give the list of filenames to put into the Google Music Player.')
     parser.add_option('-P', dest='do_push', action='store_true', default = False,
                       help = 'If chosen, then push Google Music API Mobileclient credentials into the configuration database.' )
+    parser.add_option( '--noverify', dest='do_verify', action='store_false', default = True,
+                       help = 'If chosen, do not verify SSL connections.' )
     opts, args = parser.parse_args()
     if not opts.do_push:
         if opts.filenames is None:
@@ -32,10 +46,27 @@ if __name__=='__main__':
             fnames = _files_from_glob(opts.filenames)
         else:
             fnames = _files_from_commas(opts.filenames)
-        plexmusic.upload_to_gmusic( fnames )
+        plexmusic.upload_to_gmusic( fnames, verify = opts.do_verify )
     else:
-        pass
+        flow, url = _get_oauth_credentials( )
+        print( 'Please go to this URL in a browser window: %s' % url )
+        bs = '\n'.join([ 'After giving permission for Google services on your behalf,',
+                         'type in the access code:' ] )
+        access_code = input( bs )
+        try:
+            if opts.do_verify: http = httplib2.Http( )
+            else: http = httplib2.Http( disable_ssl_certificate_validation = True )
+            credentials = flow.step2_exchange( access_code, http = http )
+            credentials.refresh( http )
+            plexmusic.oauth_store_google_credentials( credentials )
+            print( 'Success. Stored GMusicAPI Mobileclient credentials.' )
+        except Exception as e:
+            print( "What is error: %s." % str( e ) )
+            print( "Error: invallid authorization  code." )
+            return
         #if any( map(lambda tok: tok is not None, ( opts.email, opts.password ) ) ):
         #    raise ValueError( "Error, must define both Google Music email and password." )
         #plexmusic.save_gmusic_creds( opts.email.strip( ), opts.password.strip( ) )
-        
+
+if __name__=='__main__':
+    main( )

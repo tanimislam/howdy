30919e6fec42ce025a3ef906ee400aacd3fc1c51
Tanim Islam <tanim.islam@gmail.com>
2019-10-29 11:36:56 -0700

    SIGNIFICANT CHANGES WHEN USING sphinxcontrib.youtube TO SHOW VIDEOS, rather than embedded GIF files.
    FIRST, UPLOADED MP4 FILES TO YOUTUBE, GOTTEN THEIR YOUTUBE URLs
    --
    1.	plex_music_metafill_songs.mp4
    
    title:	Example of using the executable, plex_music_album.py songs mode
    
    desc:	Example of using the executable, plex_music_album.py (executable found in my Github repo, https://github.com/tanimislam/plexstuff), to create songs by specifying ARTIST and COLLECTION OF SONGS
    
    tags:	youtube, music
    
    link:	https://youtu.be/PflzMfN4A9w
    
    2.	plex_music_metafill_album.mp4
    
    title:	Example of using the executable, plex_music_album.py album mode
    
    desc:	Example of using the executable, plex_music_album.py (executable found in my Github repo, https://github.com/tanimislam/plexstuff), to create songs by specifying ARTIST and ALBUM
    
    tags:	youtube, music
    
    link:	https://youtu.be/OMu5wpb49Sw
    
    SECOND, CHANGED resources/requirements.txt AND resources/requirements.json TO INCORPORATE sphinxcontrib-youtube FROM GITHUB REPO
    --
    1. resources/requirements.txt: added lines git+https://github.com/sphinx-contrib/youtube.git#egg=sphinxcontrib-youtube
    
    2. resources/requirements.json: added key-value pair with key=git+https://github.com/sphinx-contrib/youtube.git#egg=sphinxcontrib-youtube
    
    3. convention now is that when adding a requirement that starts with git+<GIT-URL>.
       a. must have the form git+<GIT-URL>#egg=<package-name>
    
       b. the package installed git+<GIT-URL> MUST have name = <package-name>.
    
    THIRD, IMPROVED plexcore/plexinitialization.py SO THAT WORKS ON git+<URL>#egg=<package-name> entries.
    --
    1. plexcore/plexinitialization.py: enhancements to work on git+<URL>#egg=<package-name> rows.
    
    Changes to be committed:
    	modified:   docs/source/conf.py
    	modified:   docs/source/plex-music/cli_tools/plex_music_cli.rst
    	modified:   docs/source/plex-music/plex_music.rst
    	modified:   plexcore/plexinitialization.py
    	modified:   resources/requirements.json
    	modified:   resources/requirements.txt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
-    'sphinx.ext.todo'
+    'sphinx.ext.todo',
+    'sphinxcontrib.youtube'
 ]
 
 intersphinx_mapping = {

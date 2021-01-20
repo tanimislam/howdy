4ddda2a558001d409c52fd25771645e495955019
Tanim Islam <tanim.islam@gmail.com>
2021-01-19 21:35:32 -0800

    updated Sphinx documentation to use updated README.rst.

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.35-17-g3ce7735',
+    VERSION: '1.35-22-g42b2055',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

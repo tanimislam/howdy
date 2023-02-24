9bcb034ad8605bbd682f9c31be1cb4d93dd8f439
Tanim Islam <tanim.islam@gmail.com>
2023-02-23 21:58:33 -0800

    now final push to github docs.

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.35-77-gb41753fa',
+    VERSION: '1.35',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

2ed84a8b0c669046f8489cb3ec51f97fdedc7e24
Tanim Islam <tanim.islam@gmail.com>
2025-11-14 20:00:08 +0000

    more documentation fix.

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.55-92-g808fd20',
+    VERSION: '1.55-97-g65694e1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
